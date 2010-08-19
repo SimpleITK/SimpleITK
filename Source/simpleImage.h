60fd6ef61bea6f235070576888f138d5e6415875
Gabe Hart <gabe.hart@kitware.com>
2010-08-19 14:53:28 -0400

    Possible fix for templating issue

--- Source/simpleImage.h
+++ Source/simpleImage.h
@@ -5,25 +5,62 @@
 
 namespace itk {
   namespace simple {
+    
+    /**
+     * Non-templated base image class
+     */
     class Image : public LightObject {
+    
     public:
+      Image ( SimpleImageBase::Pointer image, ImageDataType datatype );
+      
+      /**
+       * Constructors for each supported pixel type (NOT DONE YET... WILL USE MACRO)
+       */
+      Image ( itk::Image<int,3>::Pointer image, ImageDataType datatype );
+      
       typedef Image Self;
       typedef SmartPointer<Self> Pointer;
-
-      //Image ( SimpleImageBase::Pointer image, ImageDataType datatype );
       
-      template<class TDataType>
-      Image ( typename itk::Image<TDataType,3>::Pointer image, ImageDataType datatype );
-      
+      /**
+       * Get a pointer to the itk::Image object
+       */
       SimpleImageBase::Pointer getITKImage();
+      
+      /**
+       * Get the image's pixel data type
+       */
       ImageDataType getImageDataType();
-
+      
+      /**
+       * Get the image's height
+       */
       unsigned long getHeight();
+      
+      /**
+       * Get the image's width
+       */
       unsigned long getWidth();
+      
+      /**
+       * Get the image's depth
+       */
       unsigned long getDepth();
-
+      
+      /**
+       * Print out a string representation of the image
+       */
       std::string toString();
-
+      
+      /**
+       * Pixel accessors for each pixel type
+       */
+      //template <class TPixelType>
+      //TPixelType getPixel(int x, int y, int z);
+      void getPixel(int x, int y, int z, float* out);
+      //uint32_t getPixel(int x, int y, int z);
+      
+      
     private:
       SimpleImageBase::Pointer mImage;
       ImageDataType mDataType;
