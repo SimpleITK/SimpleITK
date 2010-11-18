27fa629cf82238b430c1bcbd512b19ef51c5851f
Daniel Blezek <blezek.daniel@mayo.edu>
2010-11-18 10:35:09 -0600

    Fixed bug in hash calculation, updated all tests with new hashes
    In the MD5 and SHA1 calculations, the number of voxels in the buffer
    was being used, however this should have been size of the buffer in
    bytes.  Changed to use the number of voxels * sizeof the voxel data
    type.  Also fixed bug where ValueType was being used instead of the
    straight VoxelType.  We want the hash value to be computed on the
    values in memory to avoid round off error, etc.  This will also avoid
    problems cross-platform.  Hashes must be computed on the little endian
    binary data.

--- Testing/Unit/IOTests.cxx
+++ Testing/Unit/IOTests.cxx
@@ -10,16 +10,17 @@ TEST(IO,ImageFileReader) {
   MapType mapping;
 
   // Configure the mapping between filename and MD5 hash
-  mapping["Input/HeadMRVolume.mha"] = "269fea1c6db001f76b8c482e2fd3a24ef9866f9a";
-  mapping["Input/HeadMRVolumeWithDirection.nhdr"] = "269fea1c6db001f76b8c482e2fd3a24ef9866f9a";
-  mapping["Input/LittleEndian.hdr"] = "1e16b6ccb0b282069a1e278fa283efce444eeca3";
-  mapping["Input/BigEndian.hdr"] = "1e16b6ccb0b282069a1e278fa283efce444eeca3";
+  mapping["Input/HeadMRVolume.mha"] = "dd13d3cbfd470c6e3cd2ddb2302fd466";
+  mapping["Input/HeadMRVolumeWithDirection.nhdr"] = "dd13d3cbfd470c6e3cd2ddb2302fd466";
+  mapping["Input/LittleEndian.hdr"] = "61cefb1edcdef195131470f03a8238c6";
+  mapping["Input/BigEndian.hdr"] = "61cefb1edcdef195131470f03a8238c6";
 
   // Loop over the map, load each file, and compare the hash value
   for ( MapType::iterator it = mapping.begin(); it != mapping.end(); ++it ) {
     reader.SetFilename ( dataFinder.getFile ( it->first ) );
     image = reader.Execute();
     ASSERT_TRUE ( image->GetImageBase().IsNotNull() );
+    hasher.SetHashFunction ( itk::simple::ImageHashFilter::MD5 );
     EXPECT_EQ ( it->second, hasher.Execute ( image ) ) << " reading " << it->first;
   }
 
@@ -31,11 +32,17 @@ TEST(IO,ReadWrite) {
   itk::simple::ImageFileWriter writer;
   itk::simple::Image::Pointer image;
 
-  std::string md5 = "269fea1c6db001f76b8c482e2fd3a24ef9866f9a";
+  // From the command line utility
+  std::string md5 = "dd13d3cbfd470c6e3cd2ddb2302fd466";
+  std::string sha1 = "269fea1c6db001f76b8c482e2fd3a24ef9866f9a";
+
 
   image = reader.SetFilename ( dataFinder.getFile ( "Input/HeadMRVolumeWithDirection.nhdr" ) ).Execute();
   ASSERT_TRUE ( image->GetImageBase().IsNotNull() );
+  hasher.SetHashFunction ( itk::simple::ImageHashFilter::MD5 );
   EXPECT_EQ ( md5, hasher.Execute ( image ) );
+  hasher.SetHashFunction ( itk::simple::ImageHashFilter::SHA1 );
+  EXPECT_EQ ( sha1, hasher.Execute ( image ) );
 
   // Write it out
   std::string filename = dataFinder.getOutputFile ( "IO.ReadWrite.nrrd" );
@@ -43,7 +50,12 @@ TEST(IO,ReadWrite) {
   ASSERT_TRUE ( dataFinder.fileExists ( filename ) );
   image = reader.SetFilename ( filename ).Execute();
   ASSERT_TRUE ( image->GetImageBase().IsNotNull() );
+
+  // Make sure we wrote and read the file correctly
+  hasher.SetHashFunction ( itk::simple::ImageHashFilter::MD5 );
   EXPECT_EQ ( md5, hasher.Execute ( image ) );
+  hasher.SetHashFunction ( itk::simple::ImageHashFilter::SHA1 );
+  EXPECT_EQ ( sha1, hasher.Execute ( image ) );
 }
   
   
