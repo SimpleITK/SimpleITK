c72654bb6eb034f729508592a3d9772da33491f2
Luis Ibanez <luis.ibanez@kitware.com>
2010-11-24 00:11:37 -0500

    ENH: Factorizing standard code into macros.

--- Code/BasicFilters/sitkImageFilter.h
+++ Code/BasicFilters/sitkImageFilter.h
@@ -5,6 +5,23 @@
 #include "sitkMemberFunctionFactory.h"
 #include "sitkImage.h"
 
+#define sitkMemberFunctionDispatcher \
+typedef Image::Pointer (Self::*MemberFunctionType)( Image::Pointer ); \
+template <class TImageType> Image::Pointer ExecuteInternal ( Image::Pointer image ); \
+friend struct detail::MemberFunctionAddressor<MemberFunctionType>; \
+std::auto_ptr<detail::MemberFunctionFactory<MemberFunctionType> > m_MemberFactory;
+
+#define sitkSetGetMacro( type, name ) \
+  Self & Set##name( const type & value_ ) \
+    { \
+    this->m_##name = value_; \
+    return *this; \
+    } \
+  type Get##name() const \
+    { \
+    return this->m_##name; \
+    }
+
 
 namespace itk {
   namespace simple {
