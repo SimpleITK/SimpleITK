def0f7870ee55f4a6b3a64c80cd495a8e3d273cf
Daniel Blezek <blezek.daniel@mayo.edu>
2010-11-30 15:43:09 -0600

    Use the ITK exception macro for unsupported pixel types
    A naked "throw" can't be caught in C++, and will crash the program.
    Using a proper ITK exception object enables this to be nicely caught.
    Importantly, SWIG will catch this as well and convert exceptions into
    language-specific exception classes.
    
    Added option to shorten compile time by only compling a subset of the
    generated filters.

--- Code/BasicFilters/SourceCodeGenerator.cmake
+++ Code/BasicFilters/SourceCodeGenerator.cmake
@@ -1,9 +1,9 @@
+set ( GENERATED_FILTER_LIST "" CACHE INTERNAL "" )
 
 macro( expand_template FILENAME )
   # Do this in one massive custom command.  Will result in code re-generating from time to time, but that is OK (hopefully!)
   add_custom_command (
     OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/sitk${FILENAME}ImageFilter.h" "${CMAKE_CURRENT_BINARY_DIR}/sitk${FILENAME}ImageFilter.cxx"
-
     COMMAND lua ${SimpleITK_SOURCE_DIR}/Utilities/ExpandTemplate.lua ${CMAKE_CURRENT_SOURCE_DIR}/${FILENAME}.json ${CMAKE_CURRENT_SOURCE_DIR}/sitkImageFilterTemplate.h.in ${CMAKE_CURRENT_BINARY_DIR}/sitk${FILENAME}ImageFilter.h 
     COMMAND lua ${SimpleITK_SOURCE_DIR}/Utilities/ExpandTemplate.lua ${CMAKE_CURRENT_SOURCE_DIR}/${FILENAME}.json ${CMAKE_CURRENT_SOURCE_DIR}/sitkImageFilterTemplate.cxx.in ${CMAKE_CURRENT_BINARY_DIR}/sitk${FILENAME}ImageFilter.cxx 
     DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${FILENAME}.json ${CMAKE_CURRENT_SOURCE_DIR}/sitkImageFilterTemplate.h.in ${CMAKE_CURRENT_SOURCE_DIR}/sitkImageFilterTemplate.cxx.in
@@ -15,7 +15,16 @@ macro( expand_template FILENAME )
   set ( GENERATED_FILTER_LIST ${GENERATED_FILTER_LIST} ${FILENAME} CACHE INTERNAL "" )
 endmacro()
 
-file ( GLOB JSON_CONFIG_FILES *.json)
+option ( SHORT_COMPILE "Skip compiling most of the generated code." OFF )
+mark_as_advanced ( SHORT_COMPILE )
+
+if ( SHORT_COMPILE )
+  set ( JSON_CONFIG_FILES CurvatureFlow.json Subtract.json Or.json BinaryThreshold.json Add.json )
+else()
+  file ( GLOB JSON_CONFIG_FILES *.json)
+endif()
+
+
 foreach ( f ${JSON_CONFIG_FILES} ) 
   get_filename_component ( class ${f} NAME_WE )
   expand_template ( ${class} )
