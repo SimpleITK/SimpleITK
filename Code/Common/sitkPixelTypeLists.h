4ef675e9be0d17137c2ea974c1723d1434647e2d
Bradley Lowekamp <blowekamp@mail.nih.gov>
2010-12-01 13:34:54 -0500

    ENH: major revision to pixel typelists
    Redid the pixel type list to support Scalar pixels, complex numbers,
    vector images and label map images.
    
    Added a classes to define pixel types for different image types. So
    now the types in the list are not directly pixel types. This is  the
    begining of convert from pixel type to PixelIDTypes.

--- Code/Common/sitkPixelTypeLists.h
+++ Code/Common/sitkPixelTypeLists.h
@@ -3,20 +3,90 @@
 
 #include "Ancillary/TypeList.h"
 
-#include <itkRGBPixel.h>
-#include <itkRGBAPixel.h>
-#include <itkVector.h>
+#include <itkImage.h>
+#include <itkVectorImage.h>
+#include <itkLabelMap.h>
+#include <itkLabelObject.h>
+
+
+#include <complex>
 
 namespace itk
 {
 namespace simple
 {
 
-#if defined(SIMPLEITK_SHORT_COMPILE)
-// Only compile a few pixel types
-typedef typelist::MakeTypeList< unsigned char, short, int>::Type IntegerPixelTypeList; 
-typedef typelist::MakeTypeList<float>::Type RealPixelTypeList;
-#else
+template <typename TPixelType>
+struct VectorPixel
+{};
+
+
+template <typename TPixelType>
+struct LabelPixel
+{};
+
+
+
+template <typename TPixelIDType, unsigned int VImageDimension> struct PixelIDtoImageType;
+
+template <typename TPixelIDType, unsigned int VImageDimension>
+struct PixelIDtoImageType
+{
+  typedef itk::Image< TPixelIDType, VImageDimension > ImageType;
+};
+
+
+template <typename TVectorPixelType, unsigned int VImageDimension>
+struct PixelIDtoImageType< VectorPixel< TVectorPixelType >, VImageDimension >
+{
+  typedef itk::VectorImage< TVectorPixelType, VImageDimension > ImageType;
+};
+
+
+template <typename TLabelType, unsigned int VImageDimension>
+struct PixelIDtoImageType< LabelPixel< TLabelType >, VImageDimension >
+{
+  typedef itk::LabelMap< itk::LabelMap< itk::LabelObject< TLabelType, VImageDimension > > > ImageType;
+};
+
+
+template <typename TImageType> struct ImageTypeToPixelID;
+
+template <typename TPixelType, unsigned int VImageDimension>
+struct ImageTypeToPixelID< itk::Image< TPixelType, VImageDimension> >
+{
+  typedef TPixelType PixelIDType;
+};
+
+
+template <typename TPixelType, unsigned int VImageDimension>
+struct ImageTypeToPixelID< itk::VectorImage< TPixelType, VImageDimension> >
+{
+  typedef VectorPixel< TPixelType > PixelIDType;
+};
+
+
+template <typename TLabelType, unsigned int VImageDimension>
+struct ImageTypeToPixelID< itk::LabelMap< itk::LabelMap< itk::LabelObject< TLabelType, VImageDimension > > > >
+{
+  typedef  LabelPixel< TLabelType > PixelIDType;
+};
+
+
+
+
+typedef typelist::MakeTypeList<signed char,
+                               unsigned char,
+                               short,
+                               unsigned short,
+                               int,
+                               unsigned int,
+                               long,
+                               unsigned long,
+                               float,
+                               double>::Type BasicPixelTypeList;
+
+typedef BasicPixelTypeList ScalarPixelTypeList;
 
 typedef typelist::MakeTypeList<char,
                                unsigned char,
@@ -27,155 +97,35 @@ typedef typelist::MakeTypeList<char,
                                long,
                                unsigned long>::Type IntegerPixelTypeList;
 
+
 typedef typelist::MakeTypeList<float,
                                double >::Type RealPixelTypeList;
-#endif
 
-typedef typelist::Append<IntegerPixelTypeList, RealPixelTypeList>::Type BasicPixelTypeList;
+typedef typelist::MakeTypeList< std::complex< float >,
+                                std::complex< double > >::Type ComplexPixelTypeList;
 
-typedef BasicPixelTypeList ScalarPixelTypeList;
+typedef typelist::MakeTypeList<VectorPixel<signed char>,
+                               VectorPixel<char>,
+                               VectorPixel<unsigned char>,
+                               VectorPixel<short>,
+                               VectorPixel<unsigned short>,
+                               VectorPixel<float>,
+                               VectorPixel<double> >::Type VectorPixelTypeList;
 
-typedef typelist::MakeTypeList<unsigned char,
-                               unsigned short,
-                               unsigned int,
-                               unsigned long>::Type LabelPixelTypeList;
 
-typedef typelist::MakeTypeList< itk::RGBPixel<signed char>,
-                                itk::RGBPixel<unsigned char>,
-                                itk::RGBPixel<short>,
-                                itk::RGBPixel<unsigned short>,
-                                itk::RGBPixel<float>,
-                                itk::RGBPixel<double>
-                                >::Type RGBPixelTypeList;
+typedef typelist::MakeTypeList<VectorPixel<float>,
+                               VectorPixel<double> >::Type RealVectorPixelTypeList;
 
-typedef typelist::MakeTypeList< itk::RGBPixel<signed char>,
-                                itk::RGBPixel<unsigned char>,
-                                itk::RGBPixel<short>,
-                                itk::RGBPixel<unsigned short>,
-                                itk::RGBPixel<int>,
-                                itk::RGBPixel<unsigned int>,
-                                itk::RGBPixel<long>,
-                                itk::RGBPixel<unsigned long>,
-                                itk::RGBPixel<float>,
-                                itk::RGBPixel<double>
-                                >::Type AllRGBPixelTypeList;
 
-typedef typelist::MakeTypeList< itk::RGBAPixel<signed char>,
-                                itk::RGBAPixel<unsigned char>,
-                                itk::RGBAPixel<short>,
-                                itk::RGBAPixel<unsigned short>,
-                                itk::RGBAPixel<float>,
-                                itk::RGBAPixel<double>
-                                >::Type RGBAPixelTypeList;
-
-typedef typelist::MakeTypeList< itk::RGBAPixel<signed char>,
-                                itk::RGBAPixel<unsigned char>,
-                                itk::RGBAPixel<short>,
-                                itk::RGBAPixel<unsigned short>,
-                                itk::RGBAPixel<int>,
-                                itk::RGBAPixel<unsigned int>,
-                                itk::RGBAPixel<long>,
-                                itk::RGBAPixel<unsigned long>,
-                                itk::RGBAPixel<float>,
-                                itk::RGBAPixel<double>
-                                >::Type AllRGBAPixelTypeList;
+typedef typelist::MakeTypeList<LabelPixel<unsigned char>,
+                               LabelPixel<unsigned short>,
+                               LabelPixel<unsigned int>,
+                               LabelPixel<unsigned long> >::Type LabelPixelTypeList;
 
 typedef typelist::Append<
-  RGBPixelTypeList,
-  RGBAPixelTypeList
-  >::Type ColorPixelTypeList;
-
-typedef typelist::Append<
-  AllRGBPixelTypeList,
-  AllRGBAPixelTypeList
-  >::Type AllColorPixelTypeList;
-
-typedef typelist::MakeTypeList< itk::Vector<signed char,2>,
-                                itk::Vector<unsigned char,2>,
-                                itk::Vector<short,2>,
-                                itk::Vector<unsigned short,2>,
-                                itk::Vector<float,2>,
-                                itk::Vector<double,2>
-                                >::Type Vector2PixelTypeList;
-
-typedef typelist::MakeTypeList< itk::Vector<signed char,2>,
-                                itk::Vector<unsigned char,2>,
-                                itk::Vector<short,2>,
-                                itk::Vector<unsigned short,2>,
-                                itk::Vector<int,2>,
-                                itk::Vector<unsigned int,2>,
-                                itk::Vector<long,2>,
-                                itk::Vector<unsigned long,2>,
-                                itk::Vector<float,2>,
-                                itk::Vector<double,2>
-                                >::Type AllVector2PixelTypeList;
-
-typedef typelist::MakeTypeList<  itk::Vector<unsigned char,3>,
-                                itk::Vector<short,3>,
-                                itk::Vector<unsigned short,3>,
-                                itk::Vector<float,3>,
-                                itk::Vector<double,3>
-                                 >::Type Vector3PixelTypeList;
-
-typedef typelist::MakeTypeList<  itk::Vector<unsigned char,3>,
-                                itk::Vector<short,3>,
-                                itk::Vector<unsigned short,3>,
-                                itk::Vector<int,3>,
-                                itk::Vector<unsigned int,3>,
-                                itk::Vector<long,3>,
-                                itk::Vector<unsigned long,3>,
-                                itk::Vector<float,3>,
-                                itk::Vector<double,3>
-                                 >::Type AllVector3PixelTypeList;
-
-typedef typelist::MakeTypeList< itk::Vector<unsigned char,4>,
-                                itk::Vector<short,4>,
-                                itk::Vector<unsigned short,4>,
-                                itk::Vector<float,4>,
-                                itk::Vector<double,4>
-                                >::Type Vector4PixelTypeList;
-
-typedef typelist::MakeTypeList< itk::Vector<unsigned char,4>,
-                                itk::Vector<short,4>,
-                                itk::Vector<unsigned short,4>,
-                                itk::Vector<int,4>,
-                                itk::Vector<unsigned int,4>,
-                                itk::Vector<long,4>,
-                                itk::Vector<unsigned long,4>,
-                                itk::Vector<float,4>,
-                                itk::Vector<double,4>
-                                >::Type AllVector4PixelTypeList;
-
-typedef typelist::Append<
-  typelist::Append< Vector2PixelTypeList, Vector3PixelTypeList>::Type,
-  Vector4PixelTypeList
-  >::Type VectorPixelTypeList;
-
-typedef typelist::Append<
-  typelist::Append< AllVector2PixelTypeList, AllVector3PixelTypeList>::Type,
-  AllVector4PixelTypeList
-  >::Type AllVectorPixelTypeList;
-
-typedef typelist::Append<
-  typelist::Append< BasicPixelTypeList, RGBPixelTypeList>::Type,
-  typelist::Append< RGBAPixelTypeList, VectorPixelTypeList>::Type
+  typelist::Append< BasicPixelTypeList, ComplexPixelTypeList >::Type,
+  VectorPixelTypeList
   >::Type AllPixelTypeList;
-
-typedef typelist::MakeTypeList< unsigned char,
-                                short,
-                                unsigned short
-                                int,
-                                unsigned int,
-                                float,
-                                double,
-                                itk::RGBPixel<unsigned char>,
-                                itk::RGBAPixel<unsigned char>,
-                                itk::Vector<unsigned char,3>,
-                                itk::Vector<unsigned char,4>,
-                                itk::Vector<double,2>,
-                                itk::Vector<double,3>,
-                                itk::Vector<double,4>
-                                >::Type CommonPixelTypeList;
 
 }
 }
