4dc593e28d42548833ad1d09341e5e1a4048c3e5
Luis Ibanez <luis.ibanez@kitware.com>
2010-12-19 16:52:35 -0500

    ENH: First running version of SimpleGaussian.R.
    The configuration files and the example have been modified and
    a first running version was generated.
    
    There are still many manual steps related to the creating and
    installation (locally) of the SimpleITK R package. Presumably
    they could be converted into CMake custom commands.

--- Examples/SimpleGaussian.R
+++ Examples/SimpleGaussian.R
@@ -1,9 +1,14 @@
 library(SimpleITK)
 
-library() # lists the packages that are installed here.
-search()  # lists the packages that are currently loaded.
-loadedNamespaces() # lists the packages that are in namespaces.
-ls()      # lists the symbols defined so far
+myreader <- ImageFileReader()
+myreader <- ImageFileReader_SetFilename( myreader, "STAPLE1.png")
+myimage <- ImageFileReader_Execute( myreader )
 
-filename <- "lenna.png"
-reader <- ImageFileReader()
+myfilter <- RecursiveGaussianImageFilter()
+myfilter <- RecursiveGaussianImageFilter_SetSigma( myfilter, 2.0 )
+smoothedimage <- RecursiveGaussianImageFilter_Execute( myfilter, myimage )
+
+mywriter <- ImageFileWriter()
+mywriter <- ImageFileWriter_SetFilename( mywriter, "smoothedSTAPLE1.mhd" )
+mywriter <- ImageFileWriter_Execute( mywriter, smoothedimage )
+
