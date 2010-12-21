eb63fc22c6cf26327a6be620eebbfeb145d79ad2
Luis Ibanez <luis.ibanez@kitware.com>
2010-12-21 15:33:00 -0500

    ENH: Command line args following Dan's guidance.
    Before we had the filenames and sigma value hard coded.
    Dan showed us how to use command line arguments properly.

--- Examples/SimpleGaussian.R
+++ Examples/SimpleGaussian.R
@@ -1,14 +1,21 @@
+# Run with:
+#
+# Rscript --vanilla SimpleGaussian.R  input sigma output
+#
+
 library(SimpleITK)
 
+args <- commandArgs( TRUE )
+
 myreader <- ImageFileReader()
-myreader <- ImageFileReader_SetFilename( myreader, "STAPLE1.png")
+myreader <- ImageFileReader_SetFilename( myreader, args[[1]] )
 myimage <- ImageFileReader_Execute( myreader )
 
 myfilter <- RecursiveGaussianImageFilter()
-myfilter <- RecursiveGaussianImageFilter_SetSigma( myfilter, 2.0 )
+myfilter <- RecursiveGaussianImageFilter_SetSigma( myfilter, as.real(args[2]) )
 smoothedimage <- RecursiveGaussianImageFilter_Execute( myfilter, myimage )
 
 mywriter <- ImageFileWriter()
-mywriter <- ImageFileWriter_SetFilename( mywriter, "smoothedSTAPLE1.mhd" )
+mywriter <- ImageFileWriter_SetFilename( mywriter, args[[3]] )
 mywriter <- ImageFileWriter_Execute( mywriter, smoothedimage )
 
