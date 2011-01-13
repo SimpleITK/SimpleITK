70fbe447b3843128583a20b73510d36a28c469eb
Gabe Hart <gabe.hart@kitware.com>
2011-01-13 13:46:51 -0500

    REFACTOR: Replace sitkIndex with std::vector<size_t>
    Conflicts:
    
    	Code/Common/sitkMacro.h

--- Examples/Segmentation/ConnectedThresholdImageFilter.cxx
+++ Examples/Segmentation/ConnectedThresholdImageFilter.cxx
@@ -21,7 +21,6 @@
 
 
 #include "sitkImage.h"
-#include "sitkIndex.h"
 #include "sitkCurvatureFlowImageFilter.h"
 #include "sitkConnectedThresholdImageFilter.h"
 #include "sitkImageFileReader.h"
@@ -79,9 +78,13 @@ int main( int argc, char *argv[])
 
   for (int i = 5; i+1 < argc; i+=2)
     {
-    itk::simple::Index seed( atoi(argv[i]), atoi(argv[i+1]) );
+    std::vector<size_t> seed;
+    seed.push_back(atoi(argv[i]));
+    seed.push_back(atoi(argv[i+1]));
     segmentationFilter.AddSeed(seed);
-    std::cout << "Adding a seed at " << seed << std::endl;
+    std::cout << "Adding a seed at ";
+    printStdVector(seed, std::cout);
+    std::cout << std::endl;
     }
 
   itk::simple::Image::Pointer outImage = segmentationFilter.Execute(image);
