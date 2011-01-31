897a6f493280714503a62d8db3d27f4d11cea735
Daniel Blezek <blezek.daniel@mayo.edu>
2011-01-31 15:10:53 -0600

    ENH: Removed cast from Gaussian test, added functional version
    Removed cast of output from the SimpleGaussian.cxx, to bring it more
    in line with the other examples.  Added a version using the functional
    API.

--- Testing/Unit/CXXTests.cxx
+++ Testing/Unit/CXXTests.cxx
@@ -31,6 +31,31 @@ TEST_F(CXX,SimpleGaussian) {
 
   image = reader.SetFileName ( output ).Execute();
   ASSERT_TRUE ( image->GetImageBase().IsNotNull() ) << "Loaded output image";
-  EXPECT_EQ ( "9d3d4acf41aa62b453cfd6684ad7c361290462d1", hasher.Execute ( image ) );
+  EXPECT_EQ ( "de64d7d8ebfa529581f57b8c603f3d656564284f", hasher.Execute ( image ) );
+}
+
+TEST_F(CXX,SimpleGaussianFunctional) {
+  // Run the simple gaussian command line program
+  std::string output = dataFinder.GetOutputFile ( "CXX.SimpleGaussianFunctional.nrrd" );
+  std::vector<std::string> CommandLine;
+  
+  std::string exe = dataFinder.FindExecutable ( "SimpleGaussianFunctional" );
+  ASSERT_TRUE ( dataFinder.FileExists ( exe ) ) << "Couldn't find " << exe;
+
+  CommandLine.push_back ( exe );
+  CommandLine.push_back ( dataFinder.GetFile ( "Input/RA-Short.nrrd" ).c_str() );
+  CommandLine.push_back ( "2.0" );
+  CommandLine.push_back ( output );
+
+  // Run it!
+  RunExecutable ( CommandLine, true );
+  ASSERT_TRUE ( dataFinder.FileExists ( output ) );
+  itk::simple::ImageFileReader reader;
+  itk::simple::ImageHashFilter hasher;
+  itk::simple::Image::Pointer image;
+
+  image = reader.SetFileName ( output ).Execute();
+  ASSERT_TRUE ( image->GetImageBase().IsNotNull() ) << "Loaded output image";
+  EXPECT_EQ ( "de64d7d8ebfa529581f57b8c603f3d656564284f", hasher.Execute ( image ) );
 }
 
