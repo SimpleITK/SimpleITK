07c67be2c6e40a0033b9b9d8009322c69b91b3ef
Bradley Lowekamp <blowekamp@mail.nih.gov>
2011-03-31 15:26:08 -0400

    ENH: adding examples test driver for Tcl
    SIMPLEITK-44

---
+++ Testing/Unit/TclTests.cxx
@@ -0,0 +1,32 @@
+#include "SimpleITKTestHarness.h"
+
+#include <sitkImage.h>
+#include <sitkImageFileReader.h>
+#include <sitkImageFileWriter.h>
+#include <sitkHashImageFilter.h>
+
+#if defined(WRAP_TCL)
+TEST_F(Tcl,SimpleGaussian) {
+  // Run the simple gaussian command line program
+  std::string Script = dataFinder.GetSourceDirectory() + "/Examples/SimpleGaussian.tcl";
+  std::string output = dataFinder.GetOutputFile ( "Tcl.SimpleGaussian.nrrd" );
+  std::vector<std::string> CommandLine;
+  
+  CommandLine.push_back ( dataFinder.GetTclExecutable() );
+  CommandLine.push_back ( Script );
+  CommandLine.push_back ( dataFinder.GetFile ( "Input/RA-Short.nrrd" ).c_str() );
+  CommandLine.push_back ( "2.0" );
+  CommandLine.push_back ( output );
+
+  // Run it!
+  RunExecutable ( CommandLine, true );
+  ASSERT_TRUE ( dataFinder.FileExists ( output ) );
+  itk::simple::ImageFileReader reader;
+  itk::simple::HashImageFilter hasher;
+
+  itk::simple::Image image = reader.SetFileName ( output ).Execute();
+  ASSERT_TRUE ( image.GetImageBase() != NULL ) << "Loaded output image";
+  EXPECT_EQ ( "02ce020f462cf05f3c354bc33a7834603d65b906", hasher.Execute ( image ) );
+}
+
+#endif
