8cb84c8eb58881db5b895da8827005e496e8d148
Bradley Lowekamp <blowekamp@mail.nih.gov>
2011-03-31 09:42:44 -0400

    ENH: adding basic SimpleGaussian program for Tcl

---
+++ Examples/SimpleGaussian.tcl
@@ -0,0 +1,23 @@
+
+if { $argc < 3 } {
+    puts "Usage: SimpleGaussian <input> <sigma> <output>"
+    exit 1
+}
+
+ImageFileReader reader
+reader SetFileName [ lindex $argv 0 ]
+set image [ reader Execute ]
+
+set pixelID [ $image GetPixelIDValue ]
+
+RecursiveGaussianImageFilter gaussian
+gaussian SetSigma [ lindex $argv 1 ]
+set image [ gaussian Execute $image ]
+
+CastImageFilter caster
+caster SetOutputPixelType $pixelID
+set image [ caster Execute $image ]
+
+ImageFileWriter writer
+writer SetFileName [ lindex $argv 2]
+writer Execute $image
