8d7f0939012cb1cf82a5753e9a66bb3c6d1b24f4
Bradley Lowekamp <blowekamp@mail.nih.gov>
2011-03-31 15:27:40 -0400

    ENH: adding test driver for Java Examples
    The compilation of the java example happens in the driver, this is
    fairly ugly and should be revisited.
    
    SIMPLEITK-44

---
+++ Testing/Unit/JavaTests.cxx
@@ -0,0 +1,59 @@
+#include "SimpleITKTestHarness.h"
+
+#include <sitkImage.h>
+#include <sitkImageFileReader.h>
+#include <sitkImageFileWriter.h>
+#include <sitkHashImageFilter.h>
+
+#if defined(WRAP_JAVA)
+TEST_F(Java,SimpleGaussian) {
+
+  const std::string Script = dataFinder.GetSourceDirectory() + "/Examples/SimpleGaussian.java";
+
+  std::string Classpath = dataFinder.GetBuildDirectory() + "/Wrapping/org.itk.simple.jar";
+  const std::string JavaPath = dataFinder.GetExecutableDirectory();
+
+  const std::string Class = "SimpleGaussian";
+
+  // compile java program
+  {
+  std::vector<std::string> CommandLine;
+  CommandLine.push_back ( JAVAC_EXECUTABLE_PATH );
+  CommandLine.push_back ( "-classpath" );
+  CommandLine.push_back ( Classpath );
+  CommandLine.push_back ( "-d" );
+  CommandLine.push_back ( dataFinder.GetBuildDirectory() + "/Examples" );
+  CommandLine.push_back ( Script );
+
+  RunExecutable ( CommandLine, true );
+  }
+
+  // add the new class path too
+  Classpath += dataFinder.GetPathSeparator() + dataFinder.GetBuildDirectory() + "/Examples";
+
+  // Run the simple gaussian command line program
+  std::vector<std::string> CommandLine;
+
+  std::string output = dataFinder.GetOutputFile ( "Java-SimpleGaussian.nrrd" );
+  // Make sure the output is wiped
+  EXPECT_NO_THROW ( itk::simple::WriteImage( itk::simple::Image ( 1,1,1,itk::simple::sitkUInt8 ), output ) ) << "Failed to overwrite";
+
+  CommandLine.push_back ( dataFinder.GetJavaExecutable() );
+  CommandLine.push_back ( "-classpath" );
+  CommandLine.push_back ( Classpath );
+  CommandLine.push_back ( "-Djava.library.path=" + JavaPath );
+  CommandLine.push_back ( Class );
+  CommandLine.push_back ( dataFinder.GetFile ( "Input/RA-Short.nrrd" ).c_str() );
+  CommandLine.push_back ( "2.0" );
+  CommandLine.push_back ( output );
+
+
+  // Run it!
+  RunExecutable ( CommandLine, true );
+  ASSERT_TRUE ( dataFinder.FileExists ( output ) );
+
+  itk::simple::Image image = itk::simple::ReadImage( output );
+  ASSERT_TRUE ( image.GetImageBase() != NULL ) << "Loaded output image";
+  EXPECT_EQ ( "02ce020f462cf05f3c354bc33a7834603d65b906", itk::simple::Hash( image ) );
+}
+#endif
