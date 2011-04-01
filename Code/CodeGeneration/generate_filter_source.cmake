cc6bcc86b7dc5ff537c0dd1ec089a8b731a43218
Gabe Hart <gabe.hart@kitware.com>
2011-04-01 13:08:22 -0400

    REFACTOR: 12 - Further refactor json and templates
    Put json files and template files in sub-directories of Code/CodeGeneration/XXX
    
    SIMPLEITK-12

--- Code/CodeGeneration/generate_filter_source.cmake
+++ Code/CodeGeneration/generate_filter_source.cmake
@@ -1,6 +1,34 @@
 
-# include cmake macros for code generation
-include(${SimpleITK_SOURCE_DIR}/Code/CodeGeneration/expand_template.cmake)
+###############################################################################
+# This macro expands the .h and .cxx files for a given input template
+#
+macro( expand_template FILENAME input_dir output_dir library_name )
+
+  # Set common variables
+  set ( expand_template_script ${SimpleITK_SOURCE_DIR}/Utilities/ExpandTemplate.lua )
+  set ( output_h "${output_dir}/sitk${FILENAME}ImageFilter.h" )
+  set ( output_cxx "${output_dir}/sitk${FILENAME}ImageFilter.cxx" )
+
+  set ( input_json_file ${input_dir}/json/${FILENAME}.json )
+  set ( template_file_h ${input_dir}/templates/sitkImageFilterTemplate.h.in )
+  set ( template_file_cxx ${input_dir}/templates/sitkImageFilterTemplate.cxx.in )
+
+  # Make a global list of ImageFilter template filters
+  set ( IMAGE_FILTER_LIST ${IMAGE_FILTER_LIST} ${FILENAME} CACHE INTERNAL "" )
+
+  add_custom_command (
+    OUTPUT "${output_h}" "${output_cxx}"
+    COMMAND lua ${expand_template_script} code ${input_json_file} ${input_dir}/templates/sitk Template.h.in ${output_h}
+    COMMAND lua ${expand_template_script} code ${input_json_file} ${input_dir}/templates/sitk Template.cxx.in ${output_cxx}
+    DEPENDS ${input_json_file} ${template_file_h} ${template_file_cxx}
+    )
+  set ( ${library_name}GeneratedSource ${${library_name}GeneratedSource} "${output_h}" CACHE INTERNAL "" )
+  set ( ${library_name}GeneratedSource ${${library_name}GeneratedSource} "${output_cxx}" CACHE INTERNAL "" )
+
+  # Make the list visible at the global scope
+  set ( GENERATED_FILTER_LIST ${GENERATED_FILTER_LIST} ${FILENAME} CACHE INTERNAL "" )
+endmacro()
+
 
 
 ###############################################################################
@@ -19,13 +47,12 @@ macro(generate_filter_source)
   set(generated_code_output_path ${SimpleITK_BINARY_DIR}/Code/${directory_name})
 
   # Glob all json files in the current directory
-  file ( GLOB json_config_files *.json)
+  file ( GLOB json_config_files ${CMAKE_CURRENT_SOURCE_DIR}/json/*.json)
 
   # Loop through json files and expand each one
   foreach ( f ${json_config_files} )
     get_filename_component ( class ${f} NAME_WE )
     expand_template ( ${class}
-                      ${image_filter_template}
                       ${CMAKE_CURRENT_SOURCE_DIR}
                       ${generated_code_output_path}
                       SimpleITK${directory_name} )
@@ -36,7 +63,7 @@ macro(generate_filter_source)
   ######
   # Make target for generated code
   ######
-  add_custom_target(${directory_name}SourceCode ALL DEPENDS ${SimpleITK${directory_name}Source})
+  add_custom_target(${directory_name}SourceCode ALL DEPENDS ${SimpleITK${directory_name}GeneratedSource})
   if (BUILD_DOXYGEN)
     add_dependencies(Documentation ${directory_name}SourceCode)
   endif (BUILD_DOXYGEN)
