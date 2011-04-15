486f28a0df7e2d82e02d5f65d6b66285591c8f0b
Bradley Lowekamp <blowekamp@mail.nih.gov>
2011-04-15 11:26:58 -0400

    ENH: Added correct default parameters for PermuteAxes, corrected tests
    Created a static member variable which contains the correct default
    array. This is implemented in a special file which can be used for
    other definitions as well.

---
+++ Code/BasicFilters/src/sitkStaticMemberVariables.cxx
@@ -0,0 +1,14 @@
+#include "sitkPermuteAxesImageFilter.h"
+
+// This file is intended to contain the definition of static
+// membervariables needed by JSON Expand templated image filters.
+// It may also contain other member declarations, or other useful
+// items that could be specified here, as opposed to the JSON.
+
+namespace itk {
+  namespace simple {
+
+  const unsigned int PermuteAxesImageFilter::DefaultOrder[3] = {0,1,2};
+
+  }
+}
