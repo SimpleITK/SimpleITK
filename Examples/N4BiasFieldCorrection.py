9c474176ca6ca84caee0add147988f9914605bdb
Bradley Lowekamp <blowekamp@mail.nih.gov>
2011-05-03 22:02:11 -0400

    WIP: adding N4BiasFieldCorrection image filter

---
+++ Examples/N4BiasFieldCorrection.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import SimpleITK as sitk
+import sys
+
+if len ( sys.argv ) < 2:
+    print "Usage: N4BiasFieldCorrection inputImage " + \
+        "outputImage [shrinkFactor] [maskImage] [numberOfIterations] " +\
+        "[numberOfFittingLevels] [outputBiasField]"
+    sys.exit ( 1 )
+
+
+
+
+inputImage = sitk.ReadImage( sys.argv[1] )
+
+
+if len ( sys.argv ) > 5:
+    maskImage = sitk.ReadImage( sys.argv[4] )
+else:
+    maskImage = sitk.OtsuThreshold( inputImage, 0, 1, 200 )
+
+if len ( sys.argv ) > 3:
+    inputImage = sitk.Shrink( inputImage, [ int(sys.argv[3]) ] * inputImage.GetDimension() )
+    maskImage = sitk.Shrink( maskImage, [ int(sys.argv[3]) ] * inputImage.GetDimension() )
+
+corrector = sitk.N4MRIBiasFieldCorrectionImageFilter();
+
+if len ( sys.argv ) > 5:
+    corrector.SetMaximumNumberOfIterations( int( sys.argv[5] ) )
+
+if len ( sys.argv ) > 6:
+    corrector.SetNumberOfFittingLevels( int( sys.argv[6]  ) )
+
+corrector.Execute( inputImage, maskImage )    
+print corrector
