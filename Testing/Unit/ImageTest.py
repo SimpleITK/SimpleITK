2b1200e6b68a0fef12ed00864964d855fe261533
Bradley Lowekamp <blowekamp@mail.nih.gov>
2011-05-12 14:44:19 -0400

    ENH: adding python test for the image class

---
+++ Testing/Unit/ImageTest.py
@@ -0,0 +1,4 @@
+import SimpleITK as sitk
+import sys
+
+sys.exit(1)
--- Testing/Unit/PythonTests.cxx
+++ Testing/Unit/PythonTests.cxx
@@ -12,7 +12,7 @@ TEST_F(Python,SimpleGaussian) {
   std::string Script = dataFinder.GetSourceDirectory() + "/Examples/SimpleGaussian.py";
   std::string output = dataFinder.GetOutputFile ( "Python.SimpleGaussian.nrrd" );
   std::vector<std::string> CommandLine;
-  
+
   CommandLine.push_back ( dataFinder.GetPythonExecutable() );
   CommandLine.push_back ( Script );
   CommandLine.push_back ( dataFinder.GetFile ( "Input/RA-Short.nrrd" ).c_str() );
@@ -20,7 +20,7 @@ TEST_F(Python,SimpleGaussian) {
   CommandLine.push_back ( output );
 
   // Set our python path
-  std::string path = dataFinder.GetBuildDirectory() + "/Wrapping" 
+  std::string path = dataFinder.GetBuildDirectory() + "/Wrapping"
     + GetPathSeparator() + dataFinder.GetExecutableDirectory();
   SetEnvironment ( "PYTHONPATH", path );
 
@@ -28,6 +28,24 @@ TEST_F(Python,SimpleGaussian) {
   RunExecutable ( CommandLine, true );
 
   this->CheckImageHash( output, "02ce020f462cf05f3c354bc33a7834603d65b906" );
+
+}
+
+TEST_F(Python,ImageTest) {
+  // Run the simple gaussian command line program
+  std::string Script = dataFinder.GetSourceDirectory() + "/Testing/Unit/ImageTest.py";
+  std::vector<std::string> CommandLine;
+
+  CommandLine.push_back ( dataFinder.GetPythonExecutable() );
+  CommandLine.push_back ( Script );
+
+  // Set our python path
+  std::string path = dataFinder.GetBuildDirectory() + "/Wrapping"
+    + GetPathSeparator() + dataFinder.GetExecutableDirectory();
+  SetEnvironment ( "PYTHONPATH", path );
+
+  // Run it!
+  EXPECT_EQ( 0, RunExecutable ( CommandLine, true ) ) << "Failing return value.";
 
 }
 
