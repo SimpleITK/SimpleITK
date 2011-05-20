6180988bc6968e47ab9b33675609ee508faf241c
Gabe Hart <gabe.hart@kitware.com>
2011-05-20 14:28:18 -0400

    ENH:  Improved path finding of ImageJ on Mac.
    Conflicts:
    
    	Code/IO/sitkShow.cxx
    
    To resolve the conflict, I just took the version from Hans' commit -GH

--- Code/IO/sitkShow.cxx
+++ Code/IO/sitkShow.cxx
@@ -33,11 +33,11 @@ namespace itk
     {
       // Try to find ImageJ, write out a file and open
       std::vector<std::string> paths;
-      std::string ExecutableName = "ImageJ";
+      std::string ExecutableName = itksys::SystemTools::FindFile( "ImageJ" );
       std::string TempDirectory = "";
       std::string TempFile = "";
-      std::vector<std::string> CommandLine;
-#if defined(__WIN32__)
+      std::ostringstream CommandLine;
+#if defined(WIN32)
       // Windows
       // Create possible paths
       ExecutableName = "ImageJ.exe";
@@ -48,7 +48,7 @@ namespace itk
         }
 
       // Find the executable
-      ExecuteableName = itksys::SystemTools::FindFile ( ExecutableName.c_str(), paths );
+      ExecutableName = itksys::SystemTools::FindFile ( ExecutableName.c_str(), paths );
       if ( ExecutableName == "" )
         {
         sitkExceptionMacro ( << "Can not find location of " << ExecutableName );
@@ -63,136 +63,43 @@ namespace itk
         }
       TempDirectory = TempDirectory + "\\";
       TempFile = FormatFileName ( TempDirectory, name );
-      CommandLine.push_back ( "\"" + ExecutableName + "\"" );
-      CommandLine.push_back ( "-o" );
-      CommandLine.push_back ( TempFile );
+      CommandLine << "\"" + ExecutableName + "\" -o " + TempFile;
 #else
       // Handle Linux and Mac
       TempDirectory = "/tmp/";
       TempFile = FormatFileName ( TempDirectory, name );
 #if defined(__APPLE__)
-      CommandLine.push_back ( "open" );
-      CommandLine.push_back ( "-a" );
-      CommandLine.push_back ( "ImageJ" );
-      CommandLine.push_back ( TempFile );
+      paths.push_back("/Applications"); //A common place to look
+      paths.push_back("/Developer"); //A common place to look
+      paths.push_back("/opt/ImageJ");   //A common place to look
+      paths.push_back("/usr/local/ImageJ");   //A common place to look
+      ExecutableName = itksys::SystemTools::FindDirectory( "ImageJ.app" );
+      if( ExecutableName == "" )
+        {
+        // Just assume it is registered properly in a place where the open command will find it.
+        ExecutableName="ImageJ";
+        }
+      CommandLine << "open -a " << ExecutableName  << " " << TempFile;
 #else
       // Must be Linux
       ExecutableName = itksys::SystemTools::FindFile ( "ImageJ" );
-      CommandLine.push_back ( "\"" + ExecutableName + "\"" );
-      CommandLine.push_back ( "-p" );
-      CommandLine.push_back ( "1" );
-      CommandLine.push_back ( TempFile );
-      CommandLine.push_back ( "&" );
+      CommandLine << "\"" + ExecutableName + "\" -o " << TempFile << " &";
 #endif
 #endif
 
-      std::cout << "Writing image to " << TempFile << std::endl;
       WriteImage ( image, TempFile );
-
-      // Allocate temp space for the argument...  don't forget it must be null terminated
-      std::string ConcatCommand;
-      const char* StringCommandLine[256];
-      for ( unsigned int idx = 0; idx < CommandLine.size(); idx++ )
-        {
-        StringCommandLine[idx] = CommandLine[idx].c_str();
-        ConcatCommand = ConcatCommand + " " + StringCommandLine[idx];
-        }
-      StringCommandLine[CommandLine.size()] = NULL;
-
-      std::cout << "Command line " << ConcatCommand << std::endl;
-
-      system ( ConcatCommand.c_str() );
-/*
-
-      itksysProcess* process = itksysProcess_New();
-      itksysProcess_SetCommand ( process, StringCommandLine );
-      std::cout << "Set command" << std::endl;
-      itksysProcess_SetOption ( process, itksysProcess_Option_Detach, 1 );
-      itksysProcess_SetOption ( process, itksysProcess_Option_HideWindow, 1 );
-      // itksysProcess_SetOption ( process, itksysProcess_Option_Verbatim, 1 );
-      std::cout << "Set options" << std::endl;
-      itksysProcess_Execute ( process );
-      std::cout << "Execute" << std::endl;
-      // itksysProcess_Delete ( process );
-      // std::cout << "Deleted" << std::endl;
-
-    int status;
-    char* processData;
-    int processDataLength;
-
-    // Wait until we are executing
-    int ii = 0;
-    while ( 1 )
-      {
-      status = itksysProcess_GetState ( process );
-      if ( status == itksysProcess_State_Disowned )
-        {
-        std::cout << "Disowned" << std::endl;
-        // break;
-        }
-      if ( status == itksysProcess_State_Executing )
-        {
-        std::cout << "Executing" << std::endl;
-        // break;
-        }
-      if ( status == itksysProcess_State_Error )
-        {
-        std::cout << "Error" << std::endl;
-        // break;
-        }
-      if ( ii++ > 1000 )
-        {
-        break;
-        }
-      }
-      itksysProcess_Disown ( process );
-      std::cout << "Disown" << std::endl;
-
-
-    while ( 1 )
-      {
-      status = itksysProcess_WaitForData ( process, &processData, &processDataLength, NULL );
-      if ( status == itksysProcess_Pipe_STDOUT || status == itksysProcess_Pipe_STDERR )
-        {
-        std::cout << std::string ( processData ) << std::endl;
-        }
-      else
-        {
-        // Nothing ready, so wait for the process to exit...
-        break;
-        }
-      }
-
-    bool failed = false;
-    // itksysProcess_WaitForExit ( process, 0 );
-    if ( itksysProcess_GetState ( process ) == itksysProcess_State_Disowned )
-      {
-      std::cerr << "Properly disowned..." << std::endl;
-      }
-    if ( itksysProcess_GetState ( process ) == itksysProcess_State_Error )
-      {
-      std::cerr << "Error executing '" << ConcatCommand << "': " << itksysProcess_GetErrorString ( process ) << std::endl;
-      failed = true;
-      } else if ( itksysProcess_GetState ( process ) == itksysProcess_State_Exception )
-      {
-      std::cerr << "Error executing '" << ConcatCommand << "': " << itksysProcess_GetExceptionString ( process ) << std::endl;
-      failed = true;
-      }
-    itksysProcess_Delete ( process );
-*/
-/*
-      std::cout << "Execute" << std::endl;
-      itksysProcess_Disown ( process );
-      std::cout << "Disown" << std::endl;
-      itksysProcess_Delete ( process );
-      std::cout << "Deleted" << std::endl;
-*/
+      //std::cout << "Attempting: " << CommandLine.str().c_str() << std::endl;
+      system ( CommandLine.str().c_str() );
     }
 
   }
 }
 
 /*
+
 i = SimpleITK.ReadImage ( '/home/blezek/Data/Fixed.nrrd' )
 SimpleITK.Show ( i, "test" )
+
+ i = SimpleITK.ReadImage ( 'RA-Float.nrrd' )
+ SimpleITK.Show ( i )
 */
