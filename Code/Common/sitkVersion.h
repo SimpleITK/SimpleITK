1c6a91c411cc5e462fd1490b273e62c53464f7f9
Daniel Blezek <blezek.daniel@mayo.edu>
2011-06-15 15:10:20 -0500

    Added Version class, includes Git description

---
+++ Code/Common/sitkVersion.h
@@ -0,0 +1,34 @@
+#ifndef __sitkVersion_h
+#define __sitkVersion_h
+
+#include "sitkMacro.h"
+
+namespace itk
+{
+
+namespace simple
+{
+
+  /** \class Version
+   * \brief Version info for SimpleITK
+   */
+  class Version
+  {
+  public:
+    static unsigned int MajorVersion();
+    static unsigned int MinorVersion();
+    static unsigned int PatchVersion();
+
+    static std::string VersionString();
+    static std::string BuildDate();
+    static std::string GitRefspec();
+    static std::string GitSHA1();
+    static std::string GitTag();
+
+    static std::string ExtendedVersionString();
+  };
+
+}
+}
+
+#endif
---
+++ Version.cmake
@@ -0,0 +1,11 @@
+# Version info
+set(SimpleITK_Major 0)
+set(SimpleITK_Minor 0)
+set(SimpleITK_Patch 0)
+
+include(GetGitRevisionDescription)
+get_git_head_revision(GIT_REFSPEC GIT_SHA1)
+git_describe(GIT_TAG)
+
+configure_file("${CMAKE_CURRENT_SOURCE_DIR}/Code/Common/sitkVersion.cxx.in" "${CMAKE_CURRENT_BINARY_DIR}/Code/Common/sitkVersion.cxx" @ONLY)
+
