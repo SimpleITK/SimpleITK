5067bec5690a26ba5e9fb554e22a5cb07dc23f31
Bradley Lowekamp <blowekamp@mail.nih.gov>
2011-06-16 12:34:31 -0400

    BUG: Files were in wrong location.
    After the re-organization of the Code/Common directory, the version
    code needed to be placed in the correct directory.

--- Version.cmake
+++ Version.cmake
@@ -7,5 +7,5 @@ include(GetGitRevisionDescription)
 get_git_head_revision(GIT_REFSPEC GIT_SHA1)
 git_describe(GIT_TAG)
 
-configure_file("${CMAKE_CURRENT_SOURCE_DIR}/Code/Common/sitkVersion.cxx.in" "${CMAKE_CURRENT_BINARY_DIR}/Code/Common/sitkVersion.cxx" @ONLY)
+configure_file("${CMAKE_CURRENT_SOURCE_DIR}/Code/Common/src/sitkVersion.cxx.in" "${CMAKE_CURRENT_BINARY_DIR}/Code/Common/src/sitkVersion.cxx" @ONLY)
 
