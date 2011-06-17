0693409d67dea2e7294f22029fd1e8f7fdc782be
David Cole <david.cole@kitware.com>
2011-06-17 16:04:53 -0400

    ENH: CMake best practices review changes, part 2
    More improvements, as discussed in this mailing list thread:
    http://www.itk.org/mailman/private/insight-developers/2011-June/019003.html

--- Utilities/Doxygen/GenerateExamplesDox.cmake
+++ Utilities/Doxygen/GenerateExamplesDox.cmake
@@ -3,22 +3,22 @@
 #              -D "OUTPUT_FILE:PATH=${PROJECT_BINARY_DIR}/Examples.dox"
 #              -P GenerateExamplesDox.cmake 
 
-# This cmake script gets a lists of example for the project and
-# generate a file suitable for doxygen. The file is a  list of
+# This cmake script gets a lists of examples for the project and
+# generates a file suitable for doxygen. The file is a list of
 # "\examples filename", where doxygen will search for fileanme in it's
 # example path. Doxygen should then generate an examples page with the
 # referenced files.
 
 # glob the examples for each language
-file( GLOB_RECURSE EXAMPLES_CXX RELATIVE "${PROJECT_SOURCE_DIR}/Examples" "*.cxx"  )
-file( GLOB_RECURSE EXAMPLES_PYTHON RELATIVE "${PROJECT_SOURCE_DIR}/Examples" "*.py"  )
-file( GLOB_RECURSE EXAMPLES_JAVA RELATIVE "${PROJECT_SOURCE_DIR}/Examples" "*.java"  )
-file( GLOB_RECURSE EXAMPLES_TCL RELATIVE "${PROJECT_SOURCE_DIR}/Examples" "*.tcl"  )
-file( GLOB_RECURSE EXAMPLES_RUBY RELATIVE "${PROJECT_SOURCE_DIR}/Examples" "*.rb"  )
-file( GLOB_RECURSE EXAMPLES_R RELATIVE "${PROJECT_SOURCE_DIR}/Examples" "*.R"  )
-
-# append the lists together
-set( EXAMPLES_LIST "${EXAMPLES_CXX};${EXAMPLES_PYTHON};${EXAMPLES_JAVA};${EXAMPLES_TCL};${EXAMPLES_RUBY};${EXAMPLES_R}" )
+file( GLOB_RECURSE EXAMPLES_LIST
+  RELATIVE "${PROJECT_SOURCE_DIR}/Examples"
+  "*.cxx"
+  "*.py"
+  "*.java"
+  "*.tcl"
+  "*.rb"
+  "*.R"
+)
 
 # remove the file before we begin appending
 file( REMOVE ${OUTPUT_FILE} )
@@ -28,7 +28,7 @@ file( APPEND ${OUTPUT_FILE} "/**\n")
 
 FOREACH( f IN LISTS EXAMPLES_LIST )
   file( APPEND ${OUTPUT_FILE}  "\\example ${f}\n" )
-ENDFOREACH( f IN LIST EXAMPLES_LIST )
+ENDFOREACH( )
 
 # end comment
 file( APPEND ${OUTPUT_FILE}  "*/\n" )
