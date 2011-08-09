0568cc11a7bb6120b5de0d7a0330f01e02419a64
Bradley Lowekamp <blowekamp@mail.nih.gov>
2011-08-09 15:36:22 -0700

    ENH: added a test macro to use itkTestDriver for examples

--- Testing/Unit/PythonTests.cxx
+++ Testing/Unit/PythonTests.cxx
@@ -7,34 +7,6 @@
 
 #if defined(WRAP_PYTHON)
 
-TEST_F(Python,SimpleGaussian) {
-  // Run the simple gaussian command line program
-  std::string Script = dataFinder.GetSourceDirectory() + "/Examples/SimpleGaussian.py";
-  std::string output = dataFinder.GetOutputFile ( "Python.SimpleGaussian.nrrd" );
-  std::vector<std::string> CommandLine;
-
-  CommandLine.push_back ( dataFinder.GetPythonExecutable() );
-  CommandLine.push_back ( Script );
-  CommandLine.push_back ( dataFinder.GetFile ( "Input/RA-Short.nrrd" ).c_str() );
-  CommandLine.push_back ( "2.0" );
-  CommandLine.push_back ( output );
-
-  // Set our python path
-  std::string path = dataFinder.GetBuildDirectory() + "/Wrapping"
-    + GetPathSeparator() + dataFinder.GetRuntimeDirectory() + GetPathSeparator() + dataFinder.GetLibraryDirectory();
-
-  //DEBUG
-  std::cout << "PYTHONPATH = " << path << std::endl;
-
-  SetEnvironment ( "PYTHONPATH", path );
-
-  // Run it!
-  RunExecutable ( CommandLine, true );
-
-  this->CheckImageHash( output, "02ce020f462cf05f3c354bc33a7834603d65b906" );
-
-}
-
 TEST_F(Python,ImageTest) {
   // Run the simple gaussian command line program
   std::string Script = dataFinder.GetSourceDirectory() + "/Testing/Unit/ImageTest.py";
