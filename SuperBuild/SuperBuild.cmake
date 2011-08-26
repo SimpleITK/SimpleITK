5e41c72be4440c28b80fa651e30c851dfc9414d3
Bradley Lowekamp <blowekamp@mail.nih.gov>
2011-08-26 17:01:49 -0400

    ENH: change default configuration to Release

--- SuperBuild/SuperBuild.cmake
+++ SuperBuild/SuperBuild.cmake
@@ -66,6 +66,18 @@ option( ${CMAKE_PROJECT_NAME}_BUILD_TESTING "Turn on Testing for SimpleITK" ON )
 
 
 #-----------------------------------------------------------------------------
+# Set a default build type if none was specified
+if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
+  message(STATUS "Setting build type to 'Release' as none was specified.")
+  set(CMAKE_BUILD_TYPE Release CACHE STRING "Choose the type of build." FORCE)
+  # Set the possible values of build type for cmake-gui
+  set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release" "MinSizeRel" "RelWithDebInfo")
+endif()
+
+
+
+
+#-----------------------------------------------------------------------------
 # Default to build shared libraries off
 #------------------------------------------------------------------------------
 set(BUILD_SHARED_LIBS OFF)
