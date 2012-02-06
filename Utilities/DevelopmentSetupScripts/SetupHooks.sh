2b156780998570c11633c6c377cdcc7c93d983ae
Bradley Lowekamp <blowekamp@mail.nih.gov>
2012-02-06 09:11:36 -0500

    ENH: enabling hooks, without style and prefix checks
    Using the imported hooks from ITK. Modified the settings to not check
    KWStle or uncrustify. Also disabled the prefix which ITK required.
    
    Change-Id: I7fad081feeb11c1f1a2c8d63e60a8848c0318135

--- Utilities/DevelopmentSetupScripts/SetupHooks.sh
+++ Utilities/DevelopmentSetupScripts/SetupHooks.sh
@@ -49,6 +49,8 @@ if GIT_DIR=.. git for-each-ref refs/remotes/origin/hooks 2>/dev/null | \
   egrep-q 'refs/remotes/origin/hooks$'; then
   git fetch .. remotes/origin/hooks
 else
+  # we are actually going to use the ITK hooks, since we don't want to
+  # maintian separate SimpleITK ones
   git fetch http://public.kitware.com/ITK.git hooks
 fi &&
 git reset --hard FETCH_HEAD || die "Failed to install hooks"
@@ -58,20 +60,16 @@ cd ../..
 # We have a check that blocks addition of submodules.
 git config hooks.submodule false
 
-# Set up uncrustify hook.
-echo "Setting up the uncrustify hook..."
-git config hooks.uncrustify.conf "Utilities/Maintenance/uncrustify_itk.cfg"
-
+# We don't have as must style setup as ITK, but we may...
 # Set up KWStyle hook.
-echo "Setting up the KWStyle hook..."
-git config hooks.KWStyle.conf "Utilities/KWStyle/ITK.kws.xml.in"
-git config hooks.KWStyle.overwriteRulesConf "Utilities/KWStyle/ITKOverwrite.txt"
-git config hooks.KWStyle true
+git config hooks.KWStyle.conf "Utilities/KWStyle/SITK.kws.xml.in"
+git config hooks.KWStyle.overwriteRulesConf "Utilities/KWStyle/SITKOverwrite.txt"
+
+git config hooks.uncrustify false
 
 # Set up hook chaining.
-echo "Setting up ITK hook chaining: prepare-commit-msg, commit-msg, pre-commit"
+echo "Setting up SimpleITK hook chaining: prepare-commit-msg, commit-msg, pre-commit"
 git config hooks.chain-prepare-commit-msg Utilities/Hooks/prepare-commit-msg
-git config hooks.chain-commit-msg Utilities/Hooks/commit-msg
 git config hooks.chain-pre-commit Utilities/Hooks/pre-commit
 
 echo "Done."
