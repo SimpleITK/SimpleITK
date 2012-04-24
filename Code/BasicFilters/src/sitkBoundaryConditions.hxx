787926efdb5f863ff33e4e43c64b791dad0a3247
Bradley Lowekamp <blowekamp@mail.nih.gov>
2012-04-24 11:17:28 -0400

    Refactoring to simplify boundary conditions support
    Moved the switch statement for selecting the boundary condition
    to a function in a source file callable from the custom_itk_cast
    attribute for the BoundaryCondition member. Modified the json
    files accordingly.
    
    Added boundary condition choice to convolution filters
    
    The convolution filters now follow the model of the deconvolution
    filters in providing a choice of boundary conditions.
    
    Change-Id: I7566db649a6293342f82cbac46389f6a7ad92813

---
+++ Code/BasicFilters/src/sitkBoundaryConditions.hxx
@@ -0,0 +1,52 @@
+/*=========================================================================
+*
+*  Copyright Insight Software Consortium
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*         http://www.apache.org/licenses/LICENSE-2.0.txt
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+*=========================================================================*/
+#ifndef __sitkBoundaryConditions_h
+#define __sitkBoundaryConditions_h
+
+#include <itkConstantBoundaryCondition.h>
+#include <itkPeriodicBoundaryCondition.h>
+#include <itkZeroFluxNeumannBoundaryCondition.h>
+
+
+namespace itk {
+  namespace simple {
+
+  template< class TFilter, class TInternalFilter >
+  ImageBoundaryCondition< typename TInternalFilter::InputImageType > *
+  CreateNewBoundaryConditionInstance(typename TFilter::BoundaryConditionType bc)
+    {
+    switch ( bc )
+      {
+      case TFilter::ZERO_PAD:
+        return new ConstantBoundaryCondition< typename TInternalFilter::InputImageType >();
+        break;
+
+      case TFilter::PERIODIC_PAD:
+        return new PeriodicBoundaryCondition< typename TInternalFilter::InputImageType >();
+        break;
+
+      case TFilter::ZERO_FLUX_NEUMANN_PAD:
+      default:
+        return new ZeroFluxNeumannBoundaryCondition< typename TInternalFilter::InputImageType >();
+        break;
+      }
+    }
+  }
+}
+
+#endif
