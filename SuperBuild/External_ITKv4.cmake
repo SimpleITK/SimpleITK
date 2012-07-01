c9dc49fcc7c2c69e934d6fe6ca45fc0a57caa0c2
Bradley Lowekamp <blowekamp@mail.nih.gov>
2012-07-01 09:23:47 -0400

    Updating ITK git revision for VectorImageCC filter fix.

--- SuperBuild/External_ITKv4.cmake
+++ SuperBuild/External_ITKv4.cmake
@@ -16,7 +16,9 @@ set(ITK_REPOSITORY git://itk.org/ITK.git)
 
 # NOTE: it is very important to update the ITK_DIR path with the
 # current version of ITK
-set(ITK_TAG_COMMAND GIT_TAG v4.2rc04 )
+
+# this tag is post ITKv4.2rc04
+set(ITK_TAG_COMMAND GIT_TAG 710376dca4848d16af928489ac18ca7d9c65c2ae )
 
 if( ${ITK_WRAPPING} OR ${BUILD_SHARED_LIBS} )
   set( ITK_BUILD_SHARED_LIBS ON )
