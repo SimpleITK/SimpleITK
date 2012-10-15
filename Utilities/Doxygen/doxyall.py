1ac2b31da264d27f52e945add737c92625f9af80
Dave Chen <dchen@mail.nih.gov>
2012-10-15 11:26:48 -0400

    Checking 2 scripts to generate SWIG docs from SITK
    doxy2swig.py is a modified version of the one in
    /playpen/tmp/SimpleITK-build/ITK/Wrapping/Generators/Doc
    I changed it to also produce JavaDoc output.
    
    doxyall.py is a script that produces the .i file for
    all of SimpleITK.  It can either produce Java or Python-style SWIG files.
    The script is run to updated either JavaDoc.i or PythonDocstrings.i
    in SimpleITK/Wrapping.  Run the script to see the command line options.

---
+++ Utilities/Doxygen/doxyall.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+
+import glob, os, getopt, sys
+from doxy2swig import *
+
+
+def usage():
+    print "\ndoxyall.py [options] doxygen_xml_dir output.i"
+    print ""
+    print "     -h, --help    This help message"
+    print "     -j, --java    Java style output"
+    print ""
+
+
+javaFlag = 0
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "hj", [ "help", "java", ] )
+except getopt.GetoptErr, err:
+    print str(err)
+    usage()
+    sys.exit(1)
+
+for o, a in opts:
+    if o in ("-h", "--help"):
+        usage()
+        sys.exit()
+    elif o in ("-j", "--java"):
+        javaFlag = 1
+    else:
+        assert False, "Unhandled option"
+
+
+if len(args) < 2:
+    usage()
+    exit(1);
+
+indir = args[0]
+outfile = args[1]
+
+files = glob.glob(indir+"/*.xml")
+
+tmpfile = "/tmp/doxyall.i"
+
+fout = open(outfile, "w")
+
+for x in files:
+
+    print x
+
+    try:
+        p = Doxy2SWIG(x, javaFlag)
+        p.generate()
+        p.write(tmpfile)
+
+        fin = open(tmpfile, "r")
+
+        for line in fin:
+            line2 = line.replace("itk::simple::detail::", "itk::simple::")
+            fout.write(line2)
+
+        fin.close()
+    except:
+        print "Error on file ", x, "\n"
+
+fout.close()
