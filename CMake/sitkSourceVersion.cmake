b4fd739cabf3e3a189280ea57043f9fbbb3d66fb
Bradley Lowekamp <blowekamp@mail.nih.gov>
2013-01-05 10:44:34 -0500

    BUG: RC part of tag not extracted correctly
    The number post fix from a pre-release tag should be optional. And the
    correct, cmake match is used.
    x
    SimpleITK's RC version component is not extracted from the tag.
    
    Change-Id: Ic48beb5084b3974d5b1051dfe2218d6a8c2aabf4

--- CMake/sitkSourceVersion.cmake
+++ CMake/sitkSourceVersion.cmake
@@ -24,8 +24,6 @@
 # _GIT_VERSION_DEV is defined as number of commits
 # since the projects Version.cmake file has been modified. While
 # _GIT_VERSION_POST is defined as the number of commits since the tag.
-
-# _GIT_VERSION_DEV
 #
 
 
@@ -50,7 +48,7 @@ git_describe(_GIT_TAG "--match=v*")
 
 git_commits_since("${PROJECT_SOURCE_DIR}/Version.cmake" _GIT_VERSION_COUNT)
 
-set(VERSION_REGEX "^v([0-9]+)\\.([0-9]+)+(\\.([0-9]+))?(\\.([0-9]+))?((a|b|c|rc)[0-9]+)?(-[0-9]+)?")
+set(VERSION_REGEX "^v([0-9]+)\\.([0-9]+)+(\\.([0-9]+))?(\\.([0-9]+))?((a|b|c|rc)[0-9]*)?(-[0-9]+)?")
 
 string(REGEX MATCH "${VERSION_REGEX}" _out "${_GIT_TAG}")
 
@@ -73,8 +71,8 @@ if(NOT "${CMAKE_MATCH_6}" STREQUAL "")
 elseif(DEFINED ${CMAKE_PROJECT_NAME}_VERSION_TWEAK)
   set(_GIT_VERSION_TWEAK 0)
 endif()
-if(NOT "${CMAKE_MATCH_7}" STREQUAL "")
-  set(_GIT_VERSION_RC "${CMAKE_MATCH_7}" ) # a,b,rc01 etc
+if(NOT "${CMAKE_MATCH_8}" STREQUAL "")
+  set(_GIT_VERSION_RC "${CMAKE_MATCH_8}" ) # a,b,rc01 etc
 endif()
 
 if(NOT "${CMAKE_MATCH_9}" STREQUAL "")
