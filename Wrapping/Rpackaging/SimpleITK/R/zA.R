45d9acf07482cb8f5e139fe32d1486af5018389b
Bradley Lowekamp <blowekamp@mail.nih.gov>
2013-01-18 16:20:32 -0500

    Revert "Remove old RsitkTypes"
    This reverts commit 71c5ad1c92f9527face3409b2da26b0d7703633c.
    
    Change-Id: I296c01284a7873b900810af12a0a244de9c51b6c

--- Wrapping/Rpackaging/SimpleITK/R/zA.R
+++ Wrapping/Rpackaging/SimpleITK/R/zA.R
@@ -1,6 +1,67 @@
 .onLoad <- function(lib,pkg) {
   library.dynam("SimpleITK",pkg,lib)
 
+  defineEnumeration('_itk__simple__PixelIDValueEnum',
+                    .values = c(
+                      'sitkUnknown' = -1,
+                      'sitkUInt8' = RsitkUInt8(),
+                      'sitkInt8' = RsitkInt8(),
+                      'sitkUInt16' = RsitkUInt16(),
+                      'sitkInt16' = RsitkInt16(),
+                      'sitkUInt32' = RsitkUInt32(),
+                      'sitkInt32' = RsitkInt32(),
+                      'sitkUInt64' = RsitkUInt64(),
+                      'sitkInt64' = RsitkInt64(),
+                      'sitkFloat32' = RsitkFloat32(),
+                      'sitkFloat64' = RsitkFloat64(),
+                      'sitkComplexFloat32' = RsitkComplexFloat32(),
+                      'sitkComplexFloat64' = RsitkComplexFloat64(),
+                      'sitkVectorUInt8' = RsitkVectorUInt8(),
+                      'sitkVectorInt8' = RsitkVectorInt8(),
+                      'sitkVectorUInt16' = RsitkVectorUInt16(),
+                      'sitkVectorInt16' = RsitkVectorInt16(),
+                      'sitkVectorUInt32' = RsitkVectorUInt32(),
+                      'sitkVectorInt32' = RsitkVectorInt32(),
+                      'sitkVectorUInt64' = RsitkVectorUInt64(),
+                      'sitkVectorInt64' = RsitkVectorInt64(),
+                      'sitkVectorFloat32' = RsitkVectorFloat32(),
+                      'sitkVectorFloat64' = RsitkVectorFloat64(),
+                      'sitkLabelUInt8' = RsitkLabelUInt8(),
+                      'sitkLabelUInt16' = RsitkLabelUInt16(),
+                      'sitkLabelUInt32' = RsitkLabelUInt32(),
+                      'sitkLabelUInt64' = RsitkLabelUInt64()
+                      ))
+
+  defineEnumeration('_itk__simple__PixelGetEnum',
+                    .values = c(
+                      'sitkUnknown' = -1,
+                      'Image_GetPixelAsUInt8' = RsitkUInt8(),
+                      'Image_GetPixelAsInt8' = RsitkInt8(),
+                      'Image_GetPixelAsiUInt16' = RsitkUInt16(),
+                      'Image_GetPixelAsInt16' = RsitkInt16(),
+                      'Image_GetPixelAsUInt32' = RsitkUInt32(),
+                      'Image_GetPixelAsInt32' = RsitkInt32(),
+                      'Image_GetPixelAsUInt64' = RsitkUInt64(),
+                      'Image_GetPixelAsInt64' = RsitkInt64(),
+                      'Image_GetPixelAsFloat' = RsitkFloat32(),
+                      'Image_GetPixelAsFloat' = RsitkFloat64()
+                      ))
+
+  defineEnumeration('_itk__simple__PixelSetEnum',
+                    .values = c(
+                      'sitkUnknown' = -1,
+                      'Image_SetPixelAsUInt8' = RsitkUInt8(),
+                      'Image_SetPixelAsInt8' = RsitkInt8(),
+                      'Image_SetPixelAsiUInt16' = RsitkUInt16(),
+                      'Image_SetPixelAsInt16' = RsitkInt16(),
+                      'Image_SetPixelAsUInt32' = RsitkUInt32(),
+                      'Image_SetPixelAsInt32' = RsitkInt32(),
+                      'Image_SetPixelAsUInt64' = RsitkUInt64(),
+                      'Image_SetPixelAsInt64' = RsitkInt64(),
+                      'Image_SetPixelAsFloat' = RsitkFloat32(),
+                      'Image_SetPixelAsFloat' = RsitkFloat64()
+                      ))
+
 }
 
                                         # experimental bracket operator for images
