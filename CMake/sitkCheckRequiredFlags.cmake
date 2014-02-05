debf194f4161adf9e37a4d7ee5bc6df5430e97be
Bradley Lowekamp <blowekamp@mail.nih.gov>
2014-02-05 14:42:39 -0500

    Enable C++11 with libc++
    Automatically add -std=c++11 to the required flags to enable the
    tr1 features needed. This is for XCode 5.0 with clang 5.0 with libc++,
    which is missing the tr1 features with the default compilation
    options.
    
    Change-Id: Ic2ec35e4db526de95ca2a80e4a4a4b23159d9cb9

---
+++ CMake/sitkCheckRequiredFlags.cmake
@@ -0,0 +1,35 @@
+#
+#
+#
+
+include(CheckCXXCompilerFlag)
+
+
+
+#
+# Check if we need to enable C++11 with a compiler flag
+#
+
+check_cxx_compiler_flag( "-std=c++11" CXX_HAS_stdcxx11)
+
+if (CXX_HAS_stdcxx11)
+
+  message(STATUS "Checking if c++11 is required...")
+  try_compile(SITK_CHECK_CXX11_REQUIRED
+    ${SimpleITK_BINARY_DIR}/CMakeTmp
+    ${SimpleITK_SOURCE_DIR}/CMake/sitk_check_cxx11_required.cxx
+    OUTPUT_VARIABLE OUTPUT)
+  if(${SITK_CHECK_CXX11_REQUIRED})
+    message(STATUS "Checking if c++11 is required... NO" )
+  else()
+    message(STATUS "Checking if c++11 is required... YES" )
+    file(APPEND ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeError.log
+      "Checking if C++11 required try_compile failed with the following output:\n"
+      "${OUTPUT}\n")
+    string(FIND "${SimpleITK_REQUIRED_CXX_FLAGS}" "-std=c++11" required)
+    if(required LESS 0)
+      set(SimpleITK_REQUIRED_CXX_FLAGS "${SimpleITK_REQUIRED_CXX_FLAGS} -std=c++11")
+    endif()
+  endif()
+
+endif()
