f8ea0cb737d96dbcd625a744e93abaad72048aad
Bradley Lowekamp <blowekamp@mail.nih.gov>
2014-02-20 14:38:49 -0500

    Simplify detection of using 64-bit ids
    Change-Id: I44ff28702400b83b9c1b7ff43aeeb8a608514172

--- SuperBuild/External_ITK.cmake
+++ SuperBuild/External_ITK.cmake
@@ -3,22 +3,13 @@
 # Get and build itk
 
 # Determine if we need to set to use 64BITS_IDS for windows64
-include(CheckTypeSize)
-check_type_size(size_t SITK_SIZEOF_SIZE_T)
-check_type_size(long SITK_SIZEOF_LONG)
-
-set(_ITK_USE_64BITS_IDS_DEFAULT OFF)
-if(SITK_SIZEOF_SIZE_T EQUAL 8
-    AND NOT SITK_SIZEOF_LONG EQUAL 8 )
-  set(_ITK_USE_64BITS_IDS_DEFAULT ON)
+if(CMAKE_CL_64)
+  option(ITK_USE_64BITS_IDS "When ON, ITK will use 64 bits integers to
+index pixels. This is needed for managing images larger than 4Gb in
+some platforms."
+    ON)
+  mark_as_advanced(ITK_USE_64BITS_IDS)
 endif()
-
-option(ITK_USE_64BITS_IDS "When ON, ITK will use 64 bits integers to
- index pixels. This is needed for managing images larger than 4Gb in
- some platforms."
-  ${_ITK_USE_64BITS_IDS_DEFAULT})
-mark_as_advanced(ITK_USE_64BITS_IDS)
-
 
 get_cmake_property( _varNames VARIABLES )
 
