e2a84905332c59a4465d97b9e4443464053037ff
Bradley Lowekamp <blowekamp@mail.nih.gov>
2014-04-30 14:30:21 -0400

    Adding ImageRegistrationMethod1.cxx
    Change-Id: I5c4b54e91b38d93fc78cb131a8cbf2660ac6b6b6

---
+++ Examples/ImageRegistrationMethod1.cxx
@@ -0,0 +1,122 @@
+/*=========================================================================
+*
+*  Copyright Insight Software Consortium
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*         http://www.apache.org/licenses/LICENSE-2.0.txt
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+*=========================================================================*/
+
+// This one header will include all SimpleITK filters and external
+// objects.
+#include <SimpleITK.h>
+#include <iostream>
+#include <stdlib.h>
+#include <iomanip>
+
+namespace sitk = itk::simple;
+
+
+
+class IterationUpdate
+  : public sitk::Command
+{
+public:
+  IterationUpdate( const sitk::ImageRegistrationMethod &m)
+    : m_Method(m)
+    {}
+
+  virtual void Execute( )
+    {
+      // use sitk's output operator for std::vector etc..
+      using sitk::operator<<;
+
+      // stash the stream state
+      std::ios  state(NULL);
+      state.copyfmt(std::cout);
+      std::cout << std::fixed << std::setfill(' ') << std::setprecision( 5 );
+      std::cout << std::setw(3) << m_Method.GetOptimizerIteration();
+      std::cout << " = " << std::setw(10) << m_Method.GetMetricValue();
+      std::cout << " : " << m_Method.GetOptimizerPosition() << std::endl;
+
+      std::cout.copyfmt(state);
+    }
+
+private:
+  const sitk::ImageRegistrationMethod &m_Method;
+
+};
+
+
+int main(int argc, char *argv[])
+{
+
+  if ( argc < 4 )
+    {
+    std::cerr << "Usage: " << argv[0] << " <fixedImageFilter> <movingImageFile> <outputTransformFile>" << std::endl;
+    return 1;
+    }
+
+  sitk::PixelIDValueEnum pixelType = sitk::sitkFloat32;
+
+  sitk::Image fixed = sitk::ReadImage( argv[1] );
+
+  if ( fixed.GetNumberOfComponentsPerPixel() > 1 )
+    {
+    fixed = sitk::VectorIndexSelectionCast( fixed, 0, pixelType );
+    }
+  else
+    {
+    fixed = sitk::Cast( fixed, pixelType );
+    }
+
+  sitk::Image moving = sitk::ReadImage( argv[2] );
+
+  if ( moving.GetNumberOfComponentsPerPixel() > 1 )
+    {
+    moving = sitk::VectorIndexSelectionCast( moving, 0, pixelType );
+    }
+  else
+    {
+    moving = sitk::Cast( moving, pixelType );
+    }
+
+
+  sitk::ImageRegistrationMethod R;
+  R.SetMetricAsMeanSquares();
+  const double maxStep = 4.0;
+  const double minStep = 0.01;
+  const unsigned int numberOfIterations = 200;
+  const double relaxationFactor = 0.5;
+  R.SetOptimizerAsRegularStepGradientDescent( maxStep,
+                                              minStep,
+                                              numberOfIterations,
+                                              relaxationFactor );
+  R.SetTransform( sitk::Transform( fixed.GetDimension(), sitk::sitkTranslation) );
+  R.SetInterpolator( sitk::sitkLinear );
+
+  IterationUpdate cmd(R);
+  R.AddCommand( sitk::sitkIterationEvent, cmd);
+
+  sitk::Transform outTx = R.Execute( fixed, moving );
+
+  std::cout << "-------" << std::endl;
+  std::cout << outTx.ToString() << std::endl;
+  std::cout << "Optimizer stop condition: " << R.GetOptimizerStopConditionDescription() << std::endl;
+  std::cout << " Iteration: " << R.GetOptimizerIteration() << std::endl;
+  std::cout << " Metric value: " << R.GetMetricValue() << std::endl;
+
+  sitk::WriteTransform(outTx, argv[3]);
+
+
+  return 0;
+}
