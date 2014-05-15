6bd886db5b7d4421665cf5b52d276054298140c9
Bradley Lowekamp <blowekamp@mail.nih.gov>
2014-05-15 10:57:23 -0400

    BUG: Add support for vector and complex difference image
    When an image comparison fails a difference image is written to be
    used on the dashboard. For complex and vector pixel types they are now
    converted to scalar images.
    
    Change-Id: I23169e8cbda281b6a4ea9b04fa38db08e9bbfb1c

--- Testing/Unit/sitkImageCompare.cxx
+++ Testing/Unit/sitkImageCompare.cxx
@@ -42,6 +42,17 @@ void ImageCompare::NormalizeAndSave ( const sitk::Image &input, const std::strin
     {
     image = sitk::Cast( image, sitk::sitkUInt64);
     }
+  else if (image.GetPixelIDValue() == sitk::sitkComplexFloat64
+           || image.GetPixelIDValue() == sitk::sitkComplexFloat32)
+    {
+    image = sitk::ComplexToModulus(image);
+    }
+  else if (image.GetNumberOfComponentsPerPixel() != 1)
+    {
+    // just use the magnitude for vector images
+    image = sitk::VectorMagnitude(image);
+    }
+
   // Extract the center slice of our image
   if ( input.GetDimension() == 3 )
     {
@@ -54,9 +65,8 @@ void ImageCompare::NormalizeAndSave ( const sitk::Image &input, const std::strin
     image = sitk::Extract( input, sz, idx );
     }
 
-  sitk::StatisticsImageFilter stats;
-  stats.Execute ( image );
-  sitk::Image out = sitk::IntensityWindowing ( image, stats.GetMinimum(), stats.GetMaximum(), 0, 255 );
+  sitk::Image out = sitk::RescaleIntensity( image, 0, 255 );
+
   out = sitk::Cast ( out, sitk::sitkUInt8 );
   sitk::WriteImage ( out, filename );
 }
