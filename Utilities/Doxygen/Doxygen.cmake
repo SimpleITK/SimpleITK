227dabb47bfbde1c3bedb5f77650f6aa68d138f5
David T. Chen <dchen@mail.nih.gov>
2014-06-20 14:16:20 -0400

    Removed unneeded files, added option
    Using ITK's doxygen tags file can be enabled or disabled with
    the USE_ITK_DOXYGEN_TAGS option.  By default it is enabled.
    Doxygen can take a long time, if this option is enabled, especially
    if an older version of doxygen is used.
    
    Change-Id: Idf0794d6227acd55d7f8c95c11a13d42bf601eb2

--- Utilities/Doxygen/Doxygen.cmake
+++ Utilities/Doxygen/Doxygen.cmake
@@ -11,6 +11,41 @@ if (BUILD_DOXYGEN)
   #
   # Configure the script and the doxyfile, then add target
   #
+
+  option(USE_ITK_DOXYGEN_TAGS "Download ITK's Doxygen tags" ON)
+
+  if (USE_ITK_DOXYGEN_TAGS)
+
+    # Information on how to retrieve the ITK documentation tag file
+    SET(ITK_DOXYGEN_TAG_LOCATION "http://public.kitware.com/pub/itk/NightlyDoxygen/InsightDoxygenDocTag")
+    SET(ITK_DOXYGEN_COMPRESSED_TAG_FILE ${PROJECT_BINARY_DIR}/Doxygen/InsightDoxygen.tag.gz)
+    SET(ITK_DOXYGEN_TAG_FILE ${PROJECT_BINARY_DIR}/Doxygen/InsightDoxygen.tag)
+
+    # Get the ITK documentation tag file
+    IF(NOT EXISTS ${ITK_DOXYGEN_COMPRESSED_TAG_FILE})
+      FILE( DOWNLOAD
+            ${ITK_DOXYGEN_TAG_LOCATION}
+            ${ITK_DOXYGEN_COMPRESSED_TAG_FILE}
+            TIMEOUT 60
+            STATUS statusITKDoxygenTagFile
+            SHOW_PROGRESS )
+      LIST(GET statusITKDoxygenTagFile 0 statusITKDoxygenTagFile)
+      IF(statusITKDoxygenTagFile)
+        FILE(REMOVE ${ITK_DOXYGEN_COMPRESSED_TAG_FILE})
+      ENDIF()
+    ENDIF(NOT EXISTS ${ITK_DOXYGEN_COMPRESSED_TAG_FILE})
+      IF(EXISTS ${ITK_DOXYGEN_COMPRESSED_TAG_FILE})
+      FIND_PROGRAM(GZIP_TOOL NAMES gzip)
+      IF(GZIP_TOOL)
+        EXECUTE_PROCESS(COMMAND ${GZIP_TOOL} -df ${ITK_DOXYGEN_COMPRESSED_TAG_FILE})
+        SET(DOXYGEN_TAGFILES_PARAMETER "${ITK_DOXYGEN_TAG_FILE}=http://www.itk.org/Doxygen/html/")
+      ENDIF()
+    ELSE()
+      SET(DOXYGEN_TAGFILES_PARAMETER "")
+    ENDIF()
+  endif()
+
+
   configure_file(${PROJECT_SOURCE_DIR}/Utilities/Doxygen/doxygen.config.in
     ${PROJECT_BINARY_DIR}/Utilities/Doxygen/doxygen.config)
 
