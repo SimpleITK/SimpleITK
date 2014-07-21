8983a4a27ed0610023b41e6ea4f3c65e8d30c86f
Bradley Lowekamp <blowekamp@mail.nih.gov>
2014-07-21 17:39:24 -0400

    Update Python Examples and manual unit tests with standard DATA{}
    Change-Id: I45136dd943f4bd644c15478a60126b0ecff5ba71

--- Testing/Unit/AdditionalTests.cmake
+++ Testing/Unit/AdditionalTests.cmake
@@ -49,7 +49,7 @@ if ( WRAP_PYTHON )
       set(command arch -${test_arch} ${command})
     endif()
 
-    add_test(NAME Python.${name}
+    sitk_add_test(NAME Python.${name}
       COMMAND "${ITK_TEST_DRIVER}"
       --add-before-env SITK_NOSHOW "YES"
       ${command}
@@ -64,8 +64,8 @@ if ( WRAP_PYTHON )
     "${SimpleITK_SOURCE_DIR}/Examples/SimpleGaussian.py"
     --compare
       "${TEST_HARNESS_TEMP_DIRECTORY}/Python.SimpleGaussian.nrrd"
-      "${TEST_HARNESS_DATA_DIRECTORY}/Baseline/Example_SimpleGaussian_2.0.nrrd"
-    "${TEST_HARNESS_DATA_DIRECTORY}/Input/RA-Short.nrrd"
+      DATA{${SITK_DATA_ROOT}/Baseline/Example_SimpleGaussian_2.0.nrrd}
+    DATA{${SITK_DATA_ROOT}/Input/RA-Short.nrrd}
     "2.0"
     "${TEST_HARNESS_TEMP_DIRECTORY}/Python.SimpleGaussian.nrrd" )
 
@@ -73,8 +73,8 @@ if ( WRAP_PYTHON )
     "${SimpleITK_SOURCE_DIR}/Examples/SimpleGaussianProcedural.py"
     --compare
       "${TEST_HARNESS_TEMP_DIRECTORY}/Python.SimpleGaussianProcedural.nrrd"
-      "${TEST_HARNESS_DATA_DIRECTORY}/Baseline/Example_SimpleGaussian_2.0.nrrd"
-    "${TEST_HARNESS_DATA_DIRECTORY}/Input/RA-Short.nrrd"
+      DATA{${SITK_DATA_ROOT}/Baseline/Example_SimpleGaussian_2.0.nrrd}
+    DATA{${SITK_DATA_ROOT}/Input/RA-Short.nrrd}
     "2.0"
     "${TEST_HARNESS_TEMP_DIRECTORY}/Python.SimpleGaussianProcedural.nrrd" )
 
@@ -83,48 +83,48 @@ if ( WRAP_PYTHON )
 
   add_python_test( Example.CannyEdge
     "${SimpleITK_SOURCE_DIR}/Examples/CannyEdge.py"
-    "${TEST_HARNESS_DATA_DIRECTORY}/Input/OAS1_0001_MR1_mpr-1_anon.nrrd" )
+    DATA{${SITK_DATA_ROOT}/Input/OAS1_0001_MR1_mpr-1_anon.nrrd} )
 
   add_python_test( Example.ExtractSlice
     "${SimpleITK_SOURCE_DIR}/Examples/ExtractSlice.py"
-    "${TEST_HARNESS_DATA_DIRECTORY}/Input/OAS1_0001_MR1_mpr-1_anon.nrrd"
+    DATA{${SITK_DATA_ROOT}/Input/OAS1_0001_MR1_mpr-1_anon.nrrd}
     "30"
     "${TEST_HARNESS_TEMP_DIRECTORY}/Python.ExtractSlice.nrrd")
 
   add_python_test( Example.BoarderSegmentation
     "${SimpleITK_SOURCE_DIR}/Examples/BoarderSegmentation.py"
-    "${TEST_HARNESS_DATA_DIRECTORY}/Input/OAS1_0001_MR1_mpr-1_anon.nrrd"
+    DATA{${SITK_DATA_ROOT}/Input/OAS1_0001_MR1_mpr-1_anon.nrrd}
     "${TEST_HARNESS_TEMP_DIRECTORY}/Python.BoarderSegmentation.nrrd"
     "5000"
     "150" )
 
  add_python_test( Example.N4BiasFieldCorrection
     "${SimpleITK_SOURCE_DIR}/Examples/N4BiasFieldCorrection.py"
-    "${TEST_HARNESS_DATA_DIRECTORY}/Input/OAS1_0001_MR1_mpr-1_anon.nrrd"
+    DATA{${SITK_DATA_ROOT}/Input/OAS1_0001_MR1_mpr-1_anon.nrrd}
     "${TEST_HARNESS_TEMP_DIRECTORY}/Python.Example.N4BiasFieldCorrection.nrrd"
     "1" )
 
   add_python_test( Example.ReadAndShow
     "${SimpleITK_SOURCE_DIR}/Examples/ReadAndShow.py"
-    "${TEST_HARNESS_DATA_DIRECTORY}/Input/VM1111Shrink-RGB.png" )
+    DATA{${SITK_DATA_ROOT}/Input/VM1111Shrink-RGB.png} )
 
   add_python_test( Example.GeodesicActiceContourSegmentation
     "${SimpleITK_SOURCE_DIR}/Examples/GeodesicActiceContourSegmentation.py"
-    "${TEST_HARNESS_DATA_DIRECTORY}/Input/cthead1.png"
+    DATA{${SITK_DATA_ROOT}/Input/cthead1.png}
     "${TEST_HARNESS_TEMP_DIRECTORY}/Python.Example.GeodesicActiceContourSegmentation.nrrd"
     .3 10 .9 50 55
     )
 
   add_python_test( Example.FFTConvolution
     "${SimpleITK_SOURCE_DIR}/Examples/FFTConvolution.py"
-    "${TEST_HARNESS_DATA_DIRECTORY}/Input/cthead1-Float.mha"
-    "${TEST_HARNESS_DATA_DIRECTORY}/Input/Gaussian_1.5.nrrd"
+    DATA{${SITK_DATA_ROOT}/Input/cthead1-Float.mha}
+    DATA{${SITK_DATA_ROOT}/Input/Gaussian_1.5.nrrd}
     "${TEST_HARNESS_TEMP_DIRECTORY}/Python.Examples.FFTConvolution.nrrd"
     )
 
   add_python_test( Example.FilterProgressReporting
     "${SimpleITK_SOURCE_DIR}/Examples/FilterProgressReporting.py"
-    "${TEST_HARNESS_DATA_DIRECTORY}/Input/RA-Short.nrrd"
+    DATA{${SITK_DATA_ROOT}/Input/RA-Short.nrrd}
     "2.0"
     "${TEST_HARNESS_TEMP_DIRECTORY}/PythonFilterProgressReporting.nrrd"
     )
@@ -134,7 +134,7 @@ if ( WRAP_PYTHON )
     --compare-MD5
     "${TEST_HARNESS_TEMP_DIRECTORY}/DicomSeriesReader.nrrd"
     "8e65f711d450b6b466c6d76a667ecb72"
-      "${TEST_HARNESS_DATA_DIRECTORY}/Input/DicomSeries"
+      DATA{${SITK_DATA_ROOT}/Input/DicomSeries/,REGEX:Image[0-9]+.dcm}
       "${TEST_HARNESS_TEMP_DIRECTORY}/DicomSeriesReader.nrrd"
     )
 
@@ -146,7 +146,7 @@ if ( WRAP_PYTHON )
     --compare-MD5
       "${TEST_HARNESS_TEMP_DIRECTORY}/PythonSegmentationConnectedThreshold1.png"
       "d4ff9c512f9c18a84729677de800db0d"
-    "${TEST_HARNESS_DATA_DIRECTORY}/Input/BrainProtonDensitySlice.png"
+    DATA{${SITK_DATA_ROOT}/Input/BrainProtonDensitySlice.png}
     "${TEST_HARNESS_TEMP_DIRECTORY}/PythonSegmentationConnectedThreshold1.png"
     150 180
     60 116
@@ -157,7 +157,7 @@ if ( WRAP_PYTHON )
     --compare-MD5
       "${TEST_HARNESS_TEMP_DIRECTORY}/PythonSegmentationConnectedThreshold2.png"
       "a9695df04cde5b9c5481d8c253a5b3aa"
-    "${TEST_HARNESS_DATA_DIRECTORY}/Input/BrainProtonDensitySlice.png"
+    DATA{${SITK_DATA_ROOT}/Input/BrainProtonDensitySlice.png}
     "${TEST_HARNESS_TEMP_DIRECTORY}/PythonSegmentationConnectedThreshold2.png"
     210 250
     81 112
@@ -168,7 +168,7 @@ if ( WRAP_PYTHON )
     --compare-MD5
       "${TEST_HARNESS_TEMP_DIRECTORY}/PythonSegmentationNeighborhoodConnected1.png"
       "311d9d4b492e1ea625e3b0b295814dce"
-    "${TEST_HARNESS_DATA_DIRECTORY}/Input/BrainProtonDensitySlice.png"
+    DATA{${SITK_DATA_ROOT}/Input/BrainProtonDensitySlice.png}
     "${TEST_HARNESS_TEMP_DIRECTORY}/PythonSegmentationNeighborhoodConnected1.png"
     150 180
     60 116
@@ -176,38 +176,38 @@ if ( WRAP_PYTHON )
 
   add_python_test( Example.ImageRegistrationMethod1
     "${SimpleITK_SOURCE_DIR}/Examples/ImageRegistrationMethod1.py"
-    ${TEST_HARNESS_DATA_DIRECTORY}/Input/BrainProtonDensitySliceBorder20.png
-    ${TEST_HARNESS_DATA_DIRECTORY}/Input/BrainProtonDensitySliceShifted13x17y.png
+    DATA{${SITK_DATA_ROOT}/Input/BrainProtonDensitySliceBorder20.png}
+    DATA{${SITK_DATA_ROOT}/Input/BrainProtonDensitySliceShifted13x17y.png}
     ${TEST_HARNESS_TEMP_DIRECTORY}/PythonImageRegistrationMethod11Test.hdf5
     )
 
   add_python_test( Example.ImageRegistrationMethod2
     "${SimpleITK_SOURCE_DIR}/Examples/ImageRegistrationMethod2.py"
-    ${TEST_HARNESS_DATA_DIRECTORY}/Input/BrainProtonDensitySliceBorder20.png
-    ${TEST_HARNESS_DATA_DIRECTORY}/Input/BrainProtonDensitySliceShifted13x17y.png
+    DATA{${SITK_DATA_ROOT}/Input/BrainProtonDensitySliceBorder20.png}
+    DATA{${SITK_DATA_ROOT}/Input/BrainProtonDensitySliceShifted13x17y.png}
     ${TEST_HARNESS_TEMP_DIRECTORY}/PythonImageRegistrationMethod2Test.hdf5
     )
 
   add_python_test( Example.ImageRegistrationMethod4Test1
     "${SimpleITK_SOURCE_DIR}/Examples/ImageRegistrationMethod4.py"
-    ${TEST_HARNESS_DATA_DIRECTORY}/Input/BrainProtonDensitySliceBorder20.png
-    ${TEST_HARNESS_DATA_DIRECTORY}/Input/BrainProtonDensitySliceShifted13x17y.png
+    DATA{${SITK_DATA_ROOT}/Input/BrainProtonDensitySliceBorder20.png}
+    DATA{${SITK_DATA_ROOT}/Input/BrainProtonDensitySliceShifted13x17y.png}
     ${TEST_HARNESS_TEMP_DIRECTORY}/PythonImageRegistrationMethod4Test1.hdf5
     24 1.0
     )
 
   add_python_test( Example.ImageRegistrationMethod4Test2
     "${SimpleITK_SOURCE_DIR}/Examples/ImageRegistrationMethod4.py"
-    ${TEST_HARNESS_DATA_DIRECTORY}/Input/BrainT1SliceBorder20.png
-    ${TEST_HARNESS_DATA_DIRECTORY}/Input/BrainProtonDensitySliceShifted13x17y.png
+    DATA{${SITK_DATA_ROOT}/Input/BrainT1SliceBorder20.png}
+    DATA{${SITK_DATA_ROOT}/Input/BrainProtonDensitySliceShifted13x17y.png}
     ${TEST_HARNESS_TEMP_DIRECTORY}/PythonImageRegistrationMethod4Test2.hdf5
     24 0.1
     )
 
   add_python_test( Example.ImageRegistrationMethod4Test3
     "${SimpleITK_SOURCE_DIR}/Examples/ImageRegistrationMethod4.py"
-    ${TEST_HARNESS_DATA_DIRECTORY}/Input/BrainProtonDensitySliceBorder20Mask.png
-    ${TEST_HARNESS_DATA_DIRECTORY}/Input/BrainProtonDensitySliceBorder20Mask.png
+    DATA{${SITK_DATA_ROOT}/Input/BrainProtonDensitySliceBorder20Mask.png}
+    DATA{${SITK_DATA_ROOT}/Input/BrainProtonDensitySliceBorder20Mask.png}
     ${TEST_HARNESS_TEMP_DIRECTORY}/PythonImageRegistrationMethod4Test3.hdf5
     5 0.4
     )
@@ -216,16 +216,16 @@ if ( WRAP_PYTHON )
 
   add_python_test(Example.DemonsRegistration1
       "${SimpleITK_SOURCE_DIR}/Examples/DemonsRegistration1.py"
-      ${TEST_HARNESS_DATA_DIRECTORY}/Input/BrainProtonDensitySliceBorder20.png
-      ${TEST_HARNESS_DATA_DIRECTORY}/Input/BrainProtonDensitySliceBSplined10.png
+      DATA{${SITK_DATA_ROOT}/Input/BrainProtonDensitySliceBorder20.png}
+      DATA{${SITK_DATA_ROOT}/Input/BrainProtonDensitySliceBSplined10.png}
       ${TEST_HARNESS_TEMP_DIRECTORY}/PythonDemonsRegistration1Test.hdf5
     )
 
   add_python_test(Example.DemonsRegistration2
       "${SimpleITK_SOURCE_DIR}/Examples/DemonsRegistration2.py"
-      ${TEST_HARNESS_DATA_DIRECTORY}/Input/BrainProtonDensitySliceShifted13x17y.png
-      ${TEST_HARNESS_DATA_DIRECTORY}/Input/BrainProtonDensitySliceBSplined10.png
-      ${TEST_HARNESS_DATA_DIRECTORY}/Input/xforms/translation_-13x-17y.txt
+      DATA{${SITK_DATA_ROOT}/Input/BrainProtonDensitySliceShifted13x17y.png}
+      DATA{${SITK_DATA_ROOT}/Input/BrainProtonDensitySliceBSplined10.png}
+      DATA{${SITK_DATA_ROOT}/Input/xforms/translation_-13x-17y.txt}
       ${TEST_HARNESS_TEMP_DIRECTORY}/PythonDemonsRegistration2Test.hdf5
     )
 
