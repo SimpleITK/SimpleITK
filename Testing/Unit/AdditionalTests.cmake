b1ee278247c31001ceb019e2a86b6549bf4720bd
Bradley Lowekamp <blowekamp@mail.nih.gov>
2014-07-22 09:52:13 -0400

    Adding TRANSFORM_COMPARE option to sitk_add_test
    Adding arguments to sitk_add_test to compare the results of test
    program. This is useful to check the output of examples and other test
    processes which don't perform the comparison internally to the test.
    
    Change-Id: I1f21772d9f91d87b60983f9b7697af0edd176a03

--- Testing/Unit/AdditionalTests.cmake
+++ Testing/Unit/AdditionalTests.cmake
@@ -1,15 +1,37 @@
-
+include(CMakeParseArguments)
 #-----------------------------------------------------------------------------
-# SimpleITK wrapper for add_test that automatically adds testing data...
-
+# SimpleITK wrapper for add_test that adds support for external data,
+# and post comparison processes.
+#
+# ARGUMENTS
+#
+# [ADD_TEST ARGUMENTS...]
+#   The first arguments are passed to cmake's add_test function with
+#   support for the DATA{} references to external data
+# TRANSFORM_COMPARE <test transform> <baseline displacement> [tolerance]
 function(sitk_add_test)
-  # Add tests with data in the SimpleITKData group.
-  ExternalData_add_test(SimpleITKData ${ARGN})
+  set(options "")
+  set(oneValueArgs "")
+  set(multiValueArgs TRANSFORM_COMPARE)
+  cmake_parse_arguments("_" "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
+
+  # Add test with data in the SimpleITKData group.
+  ExternalData_add_test(SimpleITKData ${__UNPARSED_ARGUMENTS})
 
   if("NAME" STREQUAL "${ARGV0}")
     set(_iat_testname ${ARGV1})
   else()
     set(_iat_testname ${ARGV0})
+  endif()
+
+  if ( NOT "${__TRANSFORM_COMPARE}" STREQUAL "" )
+    ExternalData_add_test(SimpleITKData NAME "${_iat_testname}Compare"
+      COMMAND
+        $<TARGET_FILE:sitkTransformCompareDriver>
+        ${__TRANSFORM_COMPARE}
+      )
+
+    set_property(TEST "${_iat_testname}Compare" APPEND PROPERTY DEPENDS ${_iat_testname})
   endif()
 
 endfunction()
@@ -25,7 +47,15 @@ sitk_add_test(NAME TransformCompareDriver1
     $<TARGET_FILE:sitkTransformCompareDriver>
     DATA{${SITK_DATA_ROOT}/Input/xforms/translation_-13x-17y.txt}
     DATA{${SITK_DATA_ROOT}/Baseline/displacement_-13x-17y.mha}
-    )
+  )
+
+sitk_add_test(NAME TransformCompareDriver2
+  COMMAND
+    ${CMAKE_COMMAND} -E echo "Running command"
+  TRANSFORM_COMPARE
+    DATA{${SITK_DATA_ROOT}/Input/xforms/translation_-13x-17y.txt}
+    DATA{${SITK_DATA_ROOT}/Baseline/displacement_-13x-17y.mha}
+  )
 
 
 
