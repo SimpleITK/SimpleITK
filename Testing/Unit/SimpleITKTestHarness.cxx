9a4c14d4ab183f36df14432983c4f80afa4abb06
Bradley Lowekamp <blowekamp@mail.nih.gov>
2014-07-30 11:12:34 -0400

    Moving VectorDoubleRMS Predicate to cxx file
    Change-Id: Ic351b7f7c6552c24d40b7f28ab55ea72989735cd

--- Testing/Unit/SimpleITKTestHarness.cxx
+++ Testing/Unit/SimpleITKTestHarness.cxx
@@ -285,3 +285,37 @@ void CountCommand::Execute( )
 {
   ++m_Count;
 }
+
+
+
+::testing::AssertionResult VectorDoubleRMSPredFormat(const char* expr1,
+                                                     const char* expr2,
+                                                     const char* rms_error_expr,
+                                                     const std::vector<double> &val1,
+                                                     const std::vector<double> &val2,
+                                                     double rms_error) {
+  if (val1.size() != val2.size())
+    {
+    return testing::AssertionFailure()
+      << "The size of " << expr1 << " and " << expr2
+      << " different, where\n"
+      << expr1 << " evaluates to " << val1 << ",\n"
+      << expr2 << " evaluates to " << val2 << ".";
+
+    }
+  double total = 0.0;
+  for ( unsigned int i = 0; i < val1.size(); ++i )
+    {
+    double temp = (val1[i]-val2[i]);
+    total += temp*temp;
+    }
+  const double rms = sqrt(total);
+  if (rms <= rms_error) return ::testing::AssertionSuccess();
+
+  return ::testing::AssertionFailure()
+      << "The RMS difference between " << expr1 << " and " << expr2
+      << " is " << rms << ",\n  which exceeds " << rms_error_expr << ", where\n"
+      << expr1 << " evaluates to " << val1 << ",\n"
+      << expr2 << " evaluates to " << val2 << ", and\n"
+      << rms_error_expr << " evaluates to " << rms_error << ".";
+}
