91a8d3c9a8b2c4424471d10b33ab506ac0f15e63
Bradley Lowekamp <blowekamp@mail.nih.gov>
2014-08-04 13:47:57 -0400

    Adding BSpline registration examples based on ITK DeformableRegistration4
    Change-Id: Ifc6f81b4ca090b5a97e0fcd87a7dbadf1daccc2f

---
+++ Examples/ImageRegistrationMethodBSpline1.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+#=========================================================================
+#
+#  Copyright Insight Software Consortium
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0.txt
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#=========================================================================
+
+from __future__ import print_function
+
+import SimpleITK as sitk
+import sys
+import os
+
+
+def command_iteration(method) :
+    print("{0:3} = {1:10.5f}".format(method.GetOptimizerIteration(),
+                                     method.GetMetricValue()))
+
+if len ( sys.argv ) < 4:
+    print( "Usage: {0} <fixedImageFilter> <movingImageFile> <outputTransformFile>".format(sys.argv[0]))
+    sys.exit ( 1 )
+
+
+fixed = sitk.ReadImage(sys.argv[1], sitk.sitkFloat32)
+
+moving = sitk.ReadImage(sys.argv[2], sitk.sitkFloat32)
+
+transfromDomainMeshSize=[8]*moving.GetDimension()
+tx = sitk.BSplineTransformInitializer(moving,
+                                      transfromDomainMeshSize )
+
+print("Initial Parameters:");
+print(tx.GetParameters())
+
+R = sitk.ImageRegistrationMethod()
+R.SetMetricAsCorrelation()
+
+# todo review available LBFGSB optmizer parameter, match those in ITK
+# Example DeformableRegistration6.py
+R.SetOptimizerAsLBFGSB(gradientConvergenceTolerance=1e-5,
+                       maximumNumberOfIterations=1,
+                       maximumNumberOfCorrections=5,
+                       maximumNumberOfFunctionEvaluations=1000,
+                       costFunctionConvergenceFactor=1e+7)
+R.SetTransform(tx)
+R.SetInterpolator(sitk.sitkLinear)
+
+R.AddCommand( sitk.sitkIterationEvent, lambda: command_iteration(R) )
+
+outTx = R.Execute(fixed, moving)
+
+print("-------")
+print(outTx)
+print("Optimizer stop condition: {0}".format(R.GetOptimizerStopConditionDescription()))
+print(" Iteration: {0}".format(R.GetOptimizerIteration()))
+print(" Metric value: {0}".format(R.GetMetricValue()))
+
+sitk.WriteTransform(outTx,  sys.argv[3])
+
+if ( not "SITK_NOSHOW" in os.environ ):
+
+    resampler = sitk.ResampleImageFilter()
+    resampler.SetReferenceImage(fixed);
+    resampler.SetInterpolator(sitk.sitkLinear)
+    resampler.SetDefaultPixelValue(100)
+    resampler.SetTransform(outTx)
+
+    out = resampler.Execute(moving)
+    simg1 = sitk.Cast(sitk.RescaleIntensity(fixed), sitk.sitkUInt8)
+    simg2 = sitk.Cast(sitk.RescaleIntensity(out), sitk.sitkUInt8)
+    cimg = sitk.Compose(simg1, simg2, simg1/2.+simg2/2.)
+    sitk.Show( cimg, "ImageRegistration1 Composition" )
