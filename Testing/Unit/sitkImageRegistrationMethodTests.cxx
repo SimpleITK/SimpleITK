602eea1d8701f28d817c3b315e117aa50378a9de
Bradley Lowekamp <blowekamp@mail.nih.gov>
2014-08-05 16:06:25 -0400

    Adding testing of mask with registration
    Change-Id: I4be6408bf1d25b67896e6b05c4d8553c3de63c00

--- Testing/Unit/sitkImageRegistrationMethodTests.cxx
+++ Testing/Unit/sitkImageRegistrationMethodTests.cxx
@@ -21,6 +21,45 @@
 #include <stdint.h>
 #include <memory>
 
+namespace
+{
+
+
+class IterationUpdate
+  : public itk::simple::Command
+{
+public:
+  IterationUpdate( const itk::simple::ImageRegistrationMethod &m)
+    : m_Method(m)
+    {}
+
+  virtual void Execute( )
+    {
+      // use sitk's output operator for std::vector etc..
+      using itk::simple::operator<<;
+
+      // stash the stream state
+      std::ios  state(NULL);
+      state.copyfmt(std::cout);
+      std::cout << std::fixed << std::setfill(' ') << std::setprecision( 5 );
+      std::cout << std::setw(3) << m_Method.GetOptimizerIteration();
+      std::cout << " = " << std::setw(10) << m_Method.GetMetricValue();
+      std::cout << " : " << m_Method.GetOptimizerPosition() << std::endl;
+
+      std::cout.copyfmt(state);
+    }
+
+private:
+  const itk::simple::ImageRegistrationMethod &m_Method;
+
+};
+
+}
+
+//
+//
+//
+
 TEST(Registration,ImageRegistrationMethod_Basic) {
   // This test is to perform some basic coverage of methods.
 
@@ -28,4 +67,156 @@ TEST(Registration,ImageRegistrationMethod_Basic) {
 
   sitk::ImageRegistrationMethod registration;
   std::cout << registration.ToString();
+}
+
+
+//
+// Fixture based tests
+//
+
+namespace sitk = itk::simple;
+
+class sitkRegistrationMethodTest
+  : public ::testing::Test
+{
+public:
+
+  sitk::Image MakeDualGaussianBlobs(const std::vector<double> &pt0,
+                                    const std::vector<double> &pt1,
+                                    const std::vector<unsigned int> &size)
+    {
+       sitk::GaussianImageSource source1;
+      source1.SetMean(pt0);
+      source1.SetScale(1.0);
+
+      std::vector<double> sigma;
+      for(unsigned int i = 0; i < size.size(); ++i)
+        {
+        sigma.push_back(size[i]/10.0);
+        }
+      source1.SetSigma(sigma);
+      source1.SetSize(size);
+      source1.SetOutputPixelType(sitk::sitkFloat32);
+
+      sitk::GaussianImageSource source2;
+      source2.SetMean(pt1);
+      source2.SetScale(-1.0);
+      source2.SetSigma(sigma);
+      source2.SetSize(size);
+      source2.SetOutputPixelType(sitk::sitkFloat32);
+
+      return sitk::Add(source1.Execute(), source2.Execute());
+    }
+
+protected:
+  virtual void SetUp()
+  {
+    fixedBlobs = MakeDualGaussianBlobs(v2(64,64), v2(192,192), std::vector<unsigned int>(2,256));
+    movingBlobs = MakeDualGaussianBlobs(v2(54,74), v2(192,192), std::vector<unsigned int>(2,256));
+  }
+
+
+  // virtual void TearDown() {}
+
+  sitk::Image fixedBlobs;
+  sitk::Image movingBlobs;
+
+};
+
+
+TEST_F(sitkRegistrationMethodTest, Mask_Test0)
+{
+  // This test is to check some excpetional cases for using masks
+  sitk::ImageRegistrationMethod R;
+
+  R.SetOptimizerAsGradientDescent(1.0, 100);
+
+  R.SetInterpolator(sitk::sitkLinear);
+
+
+  sitk::TranslationTransform tx(fixedBlobs.GetDimension());
+  R.SetTransform(tx);
+
+  // wrong dimension should produce error
+  R.SetMetricFixedMask(sitk::Image(100,100,100,sitk::sitkUInt8));
+
+  EXPECT_THROW(R.Execute(fixedBlobs,movingBlobs), sitk::GenericException);
+
+  R.SetMetricFixedMask(sitk::Image());
+  // wrong dimension should produce error
+  R.SetMetricMovingMask(sitk::Image(100,100,100,sitk::sitkUInt8));
+
+  EXPECT_THROW(R.Execute(fixedBlobs,movingBlobs), sitk::GenericException);
+}
+
+
+TEST_F(sitkRegistrationMethodTest, Mask_Test1)
+{
+  // This test is to check that the metric masks have the correct
+  // effect.
+
+  sitk::ImageRegistrationMethod R;
+
+  double learningRate=2.0;
+  double minStep=1e-7;
+  unsigned int numberOfIterations=100;
+  double relaxationFactor=0.5;
+  double gradientMagnitudeTolerance=1e-8;
+  R.SetOptimizerAsRegularStepGradientDescent(learningRate,
+                                             minStep,
+                                             numberOfIterations,
+                                             relaxationFactor,
+                                             gradientMagnitudeTolerance );
+  R.SetInterpolator(sitk::sitkLinear);
+
+
+  sitk::TranslationTransform tx(fixedBlobs.GetDimension());
+  R.SetTransform(tx);
+
+  R.SetMetricAsCorrelation();
+  R.SetMetricFixedMask(sitk::Cast(sitk::Greater(fixedBlobs,0),sitk::sitkFloat32));
+
+  IterationUpdate cmd(R);
+  R.AddCommand(sitk::sitkIterationEvent, cmd);
+
+  sitk::Transform outTx = R.Execute(fixedBlobs,movingBlobs);
+
+  EXPECT_VECTOR_DOUBLE_NEAR(v2(-10,10), outTx.GetParameters(), 1e-4);
+}
+
+
+TEST_F(sitkRegistrationMethodTest, Mask_Test2)
+{
+  // This test is to check that the metric masks have the correct
+  // effect.
+
+  sitk::ImageRegistrationMethod R;
+
+  double learningRate=1.0;
+  double minStep=1e-7;
+  unsigned int numberOfIterations=100;
+  double relaxationFactor=0.5;
+  double gradientMagnitudeTolerance=1e-8;
+  R.SetOptimizerAsRegularStepGradientDescent(learningRate,
+                                             minStep,
+                                             numberOfIterations,
+                                             relaxationFactor,
+                                             gradientMagnitudeTolerance );
+  R.SetInterpolator(sitk::sitkLinear);
+
+
+  sitk::TranslationTransform tx(fixedBlobs.GetDimension());
+  tx.SetOffset(v2(120,99));
+  R.SetTransform(tx);
+
+  R.SetMetricAsCorrelation();
+  R.SetMetricFixedMask(sitk::Greater(fixedBlobs,0));
+  R.SetMetricMovingMask(sitk::Less(movingBlobs,0));
+
+  IterationUpdate cmd(R);
+  R.AddCommand(sitk::sitkIterationEvent, cmd);
+
+  sitk::Transform outTx = R.Execute(fixedBlobs,movingBlobs);
+
+  EXPECT_VECTOR_DOUBLE_NEAR(v2(128.0,128.0), outTx.GetParameters(), 1e-3);
 }
