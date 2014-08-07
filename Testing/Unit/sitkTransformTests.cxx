8e2d47b015568d53473850ffdb1c80edc0d7a0b3
Bradley Lowekamp <blowekamp@mail.nih.gov>
2014-08-07 12:04:45 -0400

    Adding testing for bspline order
    Change-Id: Ia03b229b57a007115e984cc11838a47f4a36e23f

--- Testing/Unit/sitkTransformTests.cxx
+++ Testing/Unit/sitkTransformTests.cxx
@@ -540,6 +540,44 @@ TEST(TransformTest,BSplineTransform)
   EXPECT_NO_THROW(tx->SetIdentity());
 }
 
+TEST(TransformTest,BSplineTransform_order)
+{
+  // test features with bspline order
+  EXPECT_THROW(sitk::BSplineTransform(4), sitk::GenericException);
+  EXPECT_THROW(sitk::BSplineTransform(2,0), sitk::GenericException);
+  EXPECT_THROW(sitk::BSplineTransform(3,0), sitk::GenericException);
+  EXPECT_THROW(sitk::BSplineTransform(2,5), sitk::GenericException);
+  EXPECT_THROW(sitk::BSplineTransform(2,20), sitk::GenericException);
+  EXPECT_THROW(sitk::BSplineTransform(3,5), sitk::GenericException);
+  EXPECT_THROW(sitk::BSplineTransform(3,99), sitk::GenericException);
+
+  std::auto_ptr<sitk::BSplineTransform> tx;
+  EXPECT_NO_THROW(tx.reset(new sitk::BSplineTransform(3)));
+  EXPECT_EQ(3, tx->GetOrder());
+  EXPECT_NO_THROW(tx.reset(new sitk::BSplineTransform(3,1)));
+  std::cout << "check\n";
+  EXPECT_EQ(1, tx->GetOrder());
+  std::cout << "check\n";
+  EXPECT_NO_THROW(tx.reset(new sitk::BSplineTransform(3,2)));
+  EXPECT_EQ(2, tx->GetOrder());
+  EXPECT_NO_THROW(tx.reset(new sitk::BSplineTransform(3,3)));
+  EXPECT_EQ(3, tx->GetOrder());
+  EXPECT_NO_THROW( tx.reset(new sitk::BSplineTransform(3,4)));
+  EXPECT_EQ(4, tx->GetOrder());
+
+  sitk::BSplineTransform tx1(*tx);
+  EXPECT_EQ(4, tx1.GetOrder());
+
+  tx.reset(new sitk::BSplineTransform(3,1));
+  EXPECT_EQ(4, tx1.GetOrder());
+  tx1 = *tx;
+  EXPECT_EQ(1, tx1.GetOrder());
+
+  // deep copy
+  tx1.SetParameters(tx1.GetParameters());
+  EXPECT_EQ(1, tx1.GetOrder());
+}
+
 
 TEST(TransformTest,Euler2DTransform)
 {
