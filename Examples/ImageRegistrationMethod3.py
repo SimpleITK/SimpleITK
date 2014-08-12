51f39ce3975c660cd0285c31a025dad488f9ca8a
Bradley Lowekamp <blowekamp@mail.nih.gov>
2014-08-12 19:08:10 -0400

    Adding example which utilizes center estimator and scales estimator
    This example also exercises, the new GetOptimizerScales.
    
    Change-Id: I818a0b70c7f55748f4202b17eeef2a1e74cc85ea

---
+++ Examples/ImageRegistrationMethod3.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+#=========================================================================
+#
+#  Copyright Insight Software Consortium
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0.txt
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#=========================================================================
+
+from __future__ import print_function
+from functools import reduce
+
+
+import SimpleITK as sitk
+import sys
+import os
+
+
+def command_iteration(method) :
+    if (method.GetOptimizerIteration()==0):
+        print("Estimated Scales: ", method.GetOptimizerScales())
+    print("{0:3} = {1:7.5f} : {2}".format(method.GetOptimizerIteration(),
+                                           method.GetMetricValue(),
+                                           method.GetOptimizerPosition()))
+
+
+
+if len ( sys.argv ) < 4:
+    print( "Usage: {0} <fixedImageFilter> <movingImageFile>  <outputTransformFile>".format(sys.argv[0]))
+    sys.exit ( 1 )
+
+pixelType = sitk.sitkFloat32
+
+fixed = sitk.ReadImage(sys.argv[1], sitk.sitkFloat32)
+
+
+moving = sitk.ReadImage(sys.argv[2], sitk.sitkFloat32)
+
+R = sitk.ImageRegistrationMethod()
+
+R.SetMetricAsCorrelation()
+
+R.SetOptimizerAsRegularStepGradientDescent(learningRate=2.0,
+                                           minStep=1e-4,
+                                           numberOfIterations=500,
+                                           gradientMagnitudeTolerance=1e-8 )
+R.SetOptimizerScalesFromIndexShift()
+
+tx = sitk.CenteredTransformInitializer(fixed, moving, sitk.Similarity2DTransform())
+R.SetInitialTransform(tx)
+
+R.SetInterpolator(sitk.sitkLinear)
+
+R.AddCommand( sitk.sitkIterationEvent, lambda: command_iteration(R) )
+
+outTx = R.Execute(fixed, moving)
+
+print("-------")
+print(outTx)
+print("Optimizer stop condition: {0}".format(R.GetOptimizerStopConditionDescription()))
+print(" Iteration: {0}".format(R.GetOptimizerIteration()))
+print(" Metric value: {0}".format(R.GetMetricValue()))
+
+
+sitk.WriteTransform(outTx,  sys.argv[3])
+
+if ( not "SITK_NOSHOW" in os.environ ):
+
+    resampler = sitk.ResampleImageFilter()
+    resampler.SetReferenceImage(fixed);
+    resampler.SetInterpolator(sitk.sitkLinear)
+    resampler.SetDefaultPixelValue(1)
+    resampler.SetTransform(outTx)
+
+    out = resampler.Execute(moving)
+
+    simg1 = sitk.Cast(sitk.RescaleIntensity(fixed), sitk.sitkUInt8)
+    simg2 = sitk.Cast(sitk.RescaleIntensity(out), sitk.sitkUInt8)
+    cimg = sitk.Compose(simg1, simg2, simg1/2.+simg2/2.)
+    sitk.Show( cimg, "ImageRegistration2 Composition" )
