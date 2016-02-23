fc60ada14e7bbcb45e54c3363f47b0e1d7512931
Bradley Lowekamp <blowekamp@mail.nih.gov>
2016-02-23 12:41:26 -0500

    Use virtualenv's setuptools and wheel, remove ez_setup
    Let virtualenv get (the potentially latest version of) setuptools and
    wheel to be installed in the virtual environment. This removes the
    need for ez_setup to obtain setuptools, and to use setuptools to
    obtain wheels.
    
    Change-Id: I8d75b863dbaaef2673f7c36da4afae036d7b3e34

--- Wrapping/Python/Packaging/setupegg.py
+++ Wrapping/Python/Packaging/setupegg.py
@@ -1,8 +1,14 @@
 """
-A setup.py script to use setuptools, which gives egg goodness, etc.
+A setup.py script to use setuptools, which gives wheel and egg goodness, etc.
 """
-from ez_setup import use_setuptools
-use_setuptools()
+try:
+    from setuptools import setup
+except ImportError:
+    # SimpleITK no longer provides ez_setup, but a copy may be around
+    # so we give it a try. It's intended that setuptools be install in
+    # a constructed virtualenv during the make process.
+    from ez_setup import use_setuptools
+    use_setuptools()
 import os
 
 from setuptools import setup
