bc5ebfc864a9cba1d7c754ba954dba4ee92ecf54
Bradley Lowekamp <blowekamp@mail.nih.gov>
2016-03-09 10:20:08 -0500

    In Python use open over file
    The file constructor has been removed in Python 3, open is required.
    
    Change-Id: I623814a2ed3c5b0c3608d4944c19f5c8f32420e2

--- Utilities/JSONValidate.py
+++ Utilities/JSONValidate.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 import json, sys
 
 try:
-    json.load( file( sys.argv[1], "r" ))
+    json.load( open( sys.argv[1], "r" ))
 except ValueError as e:
     print( "ValueError: {0}".format( e ) )
     print( "{0}:0:0: error: validating json file.".format( sys.argv[1] ) )
