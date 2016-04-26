8f7d387e39dc3b90ac4c1747c22b3ca71d75d052
Richard Beare <richard.beare@gmail.com>
2016-04-26 17:05:32 +1000

    Single functions for pixel access
    This patch creates a single SetPixel function and the
    corresponding GetPixel function. It isn't quite as nice
    as the python equivalent as the swig extend mechanism
    doesn't have an obvious way to directly add R code. Instead
    we create a dummy C function to force the appropriate
    entry in the method list and overwrite it with a hand
    generated R function in zA.R
    
    Test now covers both scalars and vectors for setting
    and getting. Label image types are also tested.
    The Get/Set methods for sitkLabelUInt8 are the
    same as for sitkUInt8.
    
    itk::simple::Unused included in dummy Set/Get methods.
    
    This patch is replacing the original, now based on
    upstream/master
    
    Change-Id: If3e613c67ee31b3b5b7af566739643103cfe0bfb

--- Testing/Unit/RPixelAccess.R
+++ Testing/Unit/RPixelAccess.R
@@ -16,9 +16,9 @@ targs <- c("sitkVectorUInt8", "sitkVectorInt8", "sitkVectorUInt16",
            "sitkVectorUInt64", "sitkVectorInt64", "sitkVectorFloat32",
            "sitkVectorFloat64")
 names(targs) <- targs
+
+
 castims <- lapply(targs, function(m)Cast(fr, m))
-res<-list()
-pixvals <- list()
 
 vectorextract <- sapply(castims, function(K)K[3,4])
 channelextract <- sapply(castims, function(K)K[[1]][3,4])
@@ -35,5 +35,69 @@ if (any(channelextract != 2) )
 {
     cat("Failure in channel extraction\n")
     cat("Pixel type: ", targs[channelextract != 2], "\n")
+    quit(save="no", status=1)
+}
+
+## Test the methods introduced via the "extend" mechanism
+
+vectorextractGet <-sapply(castims, function(K)K$GetPixel(c(3,4)-1))
+channelextractGet <- sapply(castims, function(K)K[[1]]$GetPixel(c(3,4)-1))
+
+if (any(vectorextractGet[1,] != 2) | any(vectorextractGet[2,] != 3))
+{
+    cat("Failure in vector extraction using GetPixel\n")
+    cat("Pixel type :", targs[(vectorextractGet[1,] != 2) |
+                              (vectorextractGet[2,] != 3)], "\n")
+    quit(save="no", status=1)
+}
+
+## testing reading scalar
+if (any(channelextractGet != 2) )
+{
+    cat("Failure in channel extraction\n")
+    cat("Pixel type: ", targs[channelextractGet != 2], "\n")
+    quit(save="no", status=1)
+}
+
+## Test the SetPixel method - this tests setting vector
+## recreate castims so we can write to it
+castims <- lapply(targs, function(m)Cast(fr, m))
+
+sapply(castims, function(K)K$SetPixel(c(0,0), c(10,11)))
+VecModified <- sapply(castims, function(K)K$GetPixel(c(0,0)))
+if (any(VecModified[1,] != 10) | any(VecModified[2,] != 11) )
+{
+    cat("Failure in SetPixel vector test\n")
+    cat("Pixel type: ", targs[(VecModified[1,] != 10) | (VecModified[2,] != 11)], "\n")
+    quit(save="no", status=1)
+}
+
+## Test the SetPixel method - this tests setting scalar
+## recreate castims so we can write to it
+castims <- lapply(targs, function(m)Cast(fr, m))
+
+FirstChannel <- lapply(castims, function(K)K[[1]])
+sapply(FirstChannel, function(K)K$SetPixel(c(0,0), 12))
+channelModified <- sapply(FirstChannel, function(K)K$GetPixel(c(0,0)))
+if (any(channelModified != 12) )
+{
+    cat("Failure in SetPixel scalar test\n")
+    cat("Pixel type: ", targs[channelModified != 12], "\n")
+    quit(save="no", status=1)
+}
+
+## Test label pixels
+labtargs <- c("sitkLabelUInt8", "sitkLabelUInt16", "sitkLabelUInt32", "sitkLabelUInt64")
+names(labtargs) <- labtargs
+
+labims <- lapply(labtargs, function(tt)Image(c(5,5), tt))
+
+l <- lapply(labims, function(K)K$SetPixel(c(1,2), 7))
+labvals <- sapply(labims, function(K)K$GetPixel(c(1,2)))
+
+if (any(labvals != 7))
+{
+    cat("Failure in SetPixel/GetPixel label test\n")
+    print(labvals)
     quit(save="no", status=1)
 }
