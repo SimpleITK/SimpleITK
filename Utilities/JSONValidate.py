da6b69a8c13eeedd6d2380dda0524a249a284cd5
Bradley Lowekamp <blowekamp@mail.nih.gov>
2016-05-19 14:18:26 -0400

    Enable JSONValidate to work with older pythongs
    The "except as" syntax was adding around Python2.6, causing a syntax
    error on older lines.
    
    Sometimes SimpleITK finds an older version of Python and tries to run
    the validation with it. This patch should help with those systems.
    
    Change-Id: Ic54007807e540e43cfa751ef59bcc2da0d837a6f

--- Utilities/JSONValidate.py
+++ Utilities/JSONValidate.py
@@ -4,11 +4,13 @@ import json, sys
 
 try:
     json.load( open( sys.argv[1], "r" ))
-except ValueError as e:
+except ValueError:
+    t, e = sys.exc_info()[:2] # works on old python versions
     print( "ValueError: {0}".format( e ) )
     print( "{0}:0:0: error: validating json file.".format( sys.argv[1] ) )
     sys.exit(1)
-except TypeError as e:
+except TypeError:
+    t, e = sys.exc_info()[:2]
     print( "TypeError: {0}".format( e ) )
     print( "{0}:0:0: error: validating json file.".format( sys.argv[1] ) )
     sys.exit(1)
