f1b6c05ff7723b285e69a5a84ff357a104876fc8
Bradley Lowekamp <blowekamp@mail.nih.gov>
2017-01-03 14:07:19 -0500

    update push method to https

--- simpleitk_common.cmake
+++ simpleitk_common.cmake
@@ -223,7 +223,7 @@ endif()
   # CTest delayed initialization is broken, so we put the
   # CTestConfig.cmake info here.
   set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
-  set(CTEST_DROP_METHOD "http")
+  set(CTEST_DROP_METHOD "https")
   set(CTEST_DROP_SITE "open.cdash.org")
   set(CTEST_DROP_LOCATION "submit.php?project=SimpleITK")
   set(CTEST_DROP_SITE_CDASH TRUE)
