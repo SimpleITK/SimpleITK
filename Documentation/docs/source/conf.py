791feee4d12bf83985e837e7b567b864e1237475
Bradley Lowekamp <blowekamp@mail.nih.gov>
2017-02-15 20:11:17 -0500

    disable imgmath due to compatibility issues with readthedocs
    Change-Id: I4d2c69db7bc3e35a963ca7bf2400cbe91319599e

--- Documentation/docs/source/conf.py
+++ Documentation/docs/source/conf.py
@@ -30,7 +30,8 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.imgmath',
+extensions = [
+#   'sphinx.ext.imgmath',
     'sphinx.ext.ifconfig']
 
 # Add any paths that contain templates here, relative to this directory.
