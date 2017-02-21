6e587cdfdeb795425d6f2f4b9f3085af02f881c3
Bradley Lowekamp <blowekamp@mail.nih.gov>
2017-02-21 16:55:53 -0500

    Update ITK 4.11 along release branch
    This patch includes an update to ITK's HDF5 to use CMAKE_DL_LIBS over
    directly linking to "dl".
    
    Change-Id: I5116f12fb0add37137304e554eba1372466cce0b

--- SuperBuild/External_ITK.cmake
+++ SuperBuild/External_ITK.cmake
@@ -45,7 +45,7 @@ if("${git_protocol}" STREQUAL "git")
 endif()
 
 # NOTE: it is very important to update the ITK_DIR path with the ITK version
-set(ITK_TAG_COMMAND GIT_TAG b3b8d6210b4134c7b49a41184694e7c7ff30c9e1 ) # v4.11.0+ on release branch
+set(ITK_TAG_COMMAND GIT_TAG ea1ee6165aad89e401c5f0f63220a87b4e26fa8e ) # v4.11.0+ on release branch
 
 if( ${BUILD_SHARED_LIBS} )
   set( ITK_BUILD_SHARED_LIBS ON )
