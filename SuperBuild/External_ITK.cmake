4291a577ea2ea0c56273afe549d5f7b5cb78787a
Bradley Lowekamp <blowekamp@mail.nih.gov>
2017-03-09 10:50:07 -0500

    Update ITK Superbuild version along ITK 4.11 release

--- SuperBuild/External_ITK.cmake
+++ SuperBuild/External_ITK.cmake
@@ -45,7 +45,7 @@ if("${git_protocol}" STREQUAL "git")
 endif()
 
 # NOTE: it is very important to update the ITK_DIR path with the ITK version
-set(ITK_TAG_COMMAND GIT_TAG ea1ee6165aad89e401c5f0f63220a87b4e26fa8e ) # v4.11.0+ on release branch
+set(ITK_TAG_COMMAND GIT_TAG 046ede12fe47b90c13b4fcae98b61d8b775dc4ca ) # v4.11.0+ on release branch
 
 if( ${BUILD_SHARED_LIBS} )
   set( ITK_BUILD_SHARED_LIBS ON )
