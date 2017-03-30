44c26838f72586f747911019ec43fa075adb14b9
Bradley Lowekamp <blowekamp@mail.nih.gov>
2017-03-30 14:50:11 -0400

    Rename git default variable to exclude from Superbuild propagation

--- SuperBuild/SuperBuild.cmake
+++ SuperBuild/SuperBuild.cmake
@@ -116,12 +116,12 @@ endif()
 # Use GIT protocol
 #------------------------------------------------------------------------------
 find_package(Git)
-set(SimpleITK_GIT_PROTOCOL_default "https")
+set(_SimpleITK_GIT_PROTOCOL_default "https")
 if (GIT_VERSION_STRING VERSION_LESS "1.7.10")
   # minimum version for https support
   set(SimpleITK_GIT_PROTOCOL_default "git")
 endif()
-set(SimpleITK_GIT_PROTOCOL  ${SimpleITK_GIT_PROTOCOL_default} CACHE STRING "If behind a firewall turn set this to 'https' or 'http'." )
+set(SimpleITK_GIT_PROTOCOL  ${_SimpleITK_GIT_PROTOCOL_default} CACHE STRING "If behind a firewall turn set this to 'https' or 'http'." )
 mark_as_advanced(SimpleITK_GIT_PROTOCOL)
 set_property(CACHE SimpleITK_GIT_PROTOCOL PROPERTY STRINGS "https;http;git")
 set(git_protocol ${SimpleITK_GIT_PROTOCOL})
