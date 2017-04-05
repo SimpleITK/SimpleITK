e4aa3847b7738d1d7a916c80c8dd72dae7b4dc56
Bradley Lowekamp <blowekamp@mail.nih.gov>
2017-04-05 11:35:58 -0400

    Add variable options to manylinux docker builds
    Environment variables for a SimpleITK tag and Python versions, can now
    be set by the user and propagated into the docker image for the build

--- Utilities/Distribution/manylinux/run.sh
+++ Utilities/Distribution/manylinux/run.sh
@@ -1,12 +1,23 @@
+#!/bin/sh
+
 if [ -n "$ExternalData_OBJECT_STORES" -a -d "$ExternalData_OBJECT_STORES" ] ; then
-    external_data_args="-v ${ExternalData_OBJECT_STORES}:/var/io/.ExternalData -e ExternalData_OBJECT_STORES=/var/io/.ExternalData"
+    extra_args="-v ${ExternalData_OBJECT_STORES}:/var/io/.ExternalData -e ExternalData_OBJECT_STORES=/var/io/.ExternalData"
+fi
+
+if [ ! -z "${SIMPLEITK_GIT_TAG}" ] ; then
+    extra_args="${extra_args} -e SIMPLEITK_GIT_TAG=${SIMPLEITK_GIT_TAG}"
+fi
+
+# example versions: "cp27-cp27m cp27-cp27mu cp36-cp36m"
+if [ ! -z "${PYTHON_VERSIONS}" ] ; then
+    extra_args="${extra_args} -e PYTHON_VERSIONS=${PYTHON_VERSIONS}"
 fi
 
 for ARCH in i686 x86_64; do
     docker build --pull=true  --rm=true -t simpleitk_manylinux_${ARCH} -f Dockerfile-${ARCH} .
 
-    docker run --storage-opt size=150G --rm -e _USER=$(id -un)  -e _USERID=$(id -u)  -e_GROUPID=$(id -g) $external_data_args -v $(pwd):/work/io -t simpleitk_manylinux_${ARCH}
+    docker run --storage-opt size=150G --rm -e _USER=$(id -un)  -e _USERID=$(id -u)  -e_GROUPID=$(id -g) $extra_args -v $(pwd):/work/io -t simpleitk_manylinux_${ARCH}
 
-    # use this command to debug
-    #docker run --rm -i -t --entrypoint=/bin/bash -u=root $external_data_args -v $(pwd):/var/io simpleitk_manylinux_${ARCH}
+    # use this command to get an interactive prompt to debug behavior
+    #docker run --rm -i -t --entrypoint=/bin/bash -u=root $extra_args -v $(pwd):/var/io simpleitk_manylinux_${ARCH}
 done
