c8ccb3502edc14e6c838c25ae77097440d849b31
Dave Chen <dchen@mail.nih.gov>
2017-09-15 13:52:22 -0400

    TCL version of the smiley face
    Change-Id: I8b75488992518d770fc0626c34d1f70e395c7bea

--- Examples/HelloWorld/HelloWorld.tcl
+++ Examples/HelloWorld/HelloWorld.tcl
@@ -16,8 +16,42 @@
 #
 #=========================================================================
 
-Image image 128 128 $sitkUInt16
 
-Image image2 [Add image 127]
+# Create an image
+set pixelType $sitkUInt16
+set imageSize { 128 128 }
+Image img   $imageSize $pixelType
 
-Show image2 "Hello World: TCL" 1
+# Create a face
+set faceSize   { 64 64 }
+set faceCenter { 64 64 }
+set face       [ GaussianSource $pixelType $imageSize $faceSize $faceCenter ]
+
+# Create eye images
+set eyeSize    { 5 5 }
+set eye1Center { 48 48 }
+set eye2Center { 80 48 }
+set eye1       [ GaussianSource $pixelType $imageSize $eyeSize $eye1Center 150 ]
+set eye2       [ GaussianSource $pixelType $imageSize $eyeSize $eye2Center 150 ]
+
+# Apply the eyes to the face
+set face [ Subtract $face $eye1 ]
+set face [ Subtract $face $eye2 ]
+set face [ BinaryThreshold $face 200 255 255 ]
+
+# Create the mouth
+set mouthRadii  { 30 20 }
+set mouthCenter { 64 76 }
+set mouth [ GaussianSource $pixelType $imageSize $mouthRadii $mouthCenter ]
+set mouth [ Subtract 255 [ BinaryThreshold $mouth 200 255 255 ] ]
+
+# Paste the mouth onto the face
+set mouthSize { 64 18 }
+set mouthLoc  { 32 76 }
+set face [ Paste $face $mouth $mouthSize $mouthLoc $mouthLoc ]
+
+# Apply the face to the original image
+set img $face
+
+# Display the results
+Show $img "Hello World: TCL" 1
