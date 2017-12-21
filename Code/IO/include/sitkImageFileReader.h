ee2323ece7cbb1f931b6c89d0ee9ef7c2129938c
Bradley Lowekamp <blowekamp@mail.nih.gov>
2017-12-21 16:34:16 -0500

    Add meta-data information interface to the ImageFileReader
    This new interface available only to the ImageFileReader's object
    oriented interface provides access to the image information and the
    meta-data dictionary provided by the itk ImageIO after
    ImageIOBase::ReadImageInformation is called. This provides and
    interface to obtain the size of a file and it's meta-data before the
    bulk data is loaded.

--- Code/IO/include/sitkImageFileReader.h
+++ Code/IO/include/sitkImageFileReader.h
@@ -23,7 +23,11 @@
 #include "sitkImageReaderBase.h"
 #include "sitkMemberFunctionFactory.h"
 
+
 namespace itk {
+
+  class MetaDataDictionary;
+
   namespace simple {
 
     /** \class ImageFileReader
@@ -54,9 +58,73 @@ namespace itk {
 
       ImageFileReader();
 
+      // Interface methods to access image file's meta-data and image
+      // information after calling Execute or after calling
+      // MetaDataRead, which does not load the bulk pixel data.
+
+      /** \brief Read only the meta-data and image information in the file.
+       *
+       * This method can be used to determine what the size and pixel
+       * type of an image file is with out reading the whole
+       * image. Even if SimpleITK does not support an image of a
+       * certain dimension or type, the meta-information can still be
+       * read.
+       */
+      void ReadImageInformation(void);
+
+      /** \brief Image information methods updated via ReadImageInformation
+       *
+       * These accessor methods are valid after a call to ReadImageInformation
+       * or Execute. They contain the image information from the file
+       * via the itk::ImageIO. Information such as NumberOfComponents
+       * is based of the file format, so the number of components for
+       * internal types such as RGB or complex may not be described
+       * the same as with SimpleITK's Image interface.
+       * @{
+       */
+      PixelIDValueEnum GetPixelID( void ) const;
+      PixelIDValueType GetPixelIDValue( void ) const;
+      unsigned int GetDimension( void ) const;
+      unsigned int GetNumberOfComponents( void ) const;
+      const std::vector<double> &GetOrigin( void ) const;
+      const std::vector<double> &GetSpacing( void ) const;
+      const std::vector<double> &GetDirection() const;
+      const std::vector<uint64_t> &GetSize( void ) const;
+      /* @} */
+
+      /** \brief Get the meta-data dictionary keys
+       *
+       * This is only valid after successful ReadImageInformation or Execute
+       * of this filter.
+       *
+       * Returns a vector of keys to the key/value entries in the
+       * file's meta-data dictionary. Iterate through with these keys
+       * to get the values.
+       **/
+      std::vector<std::string> GetMetaDataKeys( void ) const;
+
+      /** \brief Query a meta-data dictionary for the existence of a key.
+       **/
+      bool HasMetaDataKey( const std::string &key ) const;
+
+      /** \brief Get the value of a meta-data dictionary entry as a string.
+       *
+       * If the key is not in the dictionary then an exception is
+       * thrown.
+       *
+       * String types in the dictionary are returned as their native
+       * string. Other types are printed to string before returning.
+       **/
+      std::string GetMetaData( const std::string &key ) const;
+
     protected:
 
       template <class TImageType> Image ExecuteInternal ( itk::ImageIOBase * );
+
+      /** Internal method which update's this classes stored meta-data
+       * and image information.
+       */
+      void UpdateImageInformationFromImageIO( const itk::ImageIOBase* iobase );
 
     private:
 
@@ -67,7 +135,23 @@ namespace itk {
       friend struct detail::MemberFunctionAddressor<MemberFunctionType>;
       nsstd::auto_ptr<detail::MemberFunctionFactory<MemberFunctionType> > m_MemberFactory;
 
+
+      nsstd::function<std::vector<std::string>()> m_pfGetMetaDataKeys;
+      nsstd::function<bool(const std::string &)> m_pfHasMetaDataKey;
+      nsstd::function<std::string(const std::string &)> m_pfGetMetaData;
+
       std::string m_FileName;
+
+      nsstd::auto_ptr<MetaDataDictionary> m_MetaDataDictionary;
+
+      PixelIDValueEnum     m_PixelType;
+      unsigned int         m_Dimension;
+      unsigned int         m_NumberOfComponents;
+      std::vector<double>  m_Direction;
+      std::vector<double>  m_Origin;
+      std::vector<double>  m_Spacing;
+
+      std::vector<uint64_t> m_Size;
     };
 
   /**
