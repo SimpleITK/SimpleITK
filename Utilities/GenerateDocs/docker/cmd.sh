34337819be5bc2a01dd22128331e924a9c371526
Richard Beare <Richard.Beare@gmail.com>
2019-01-11 10:10:31 +1100

    Extended docker config to support build of python/java/R package docs

--- Utilities/GenerateDocs/docker/cmd.sh
+++ Utilities/GenerateDocs/docker/cmd.sh
@@ -5,8 +5,11 @@ set -eux
 
 OUT_DIR=/work/io
 SIMPLEITK_GIT_TAG=${SIMPLEITK_GIT_TAG:-v1.1rc1}
+SIMPLEITK_GIT_REPO=${SIMPLEITK_GIT_REPO:-https://github.com/SimpleITK/SimpleITK.git}
 
-git clone https://github.com/SimpleITK/SimpleITK.git && ( cd SimpleITK && git checkout ${SIMPLEITK_GIT_TAG} )
+git clone  ${SIMPLEITK_GIT_REPO} && ( cd SimpleITK && git checkout ${SIMPLEITK_GIT_TAG} )
+
+here=$(pwd)
 
 mkdir bld && cd bld
 
@@ -20,4 +23,26 @@ cmake \
 ninja SimpleITK-doc
 
 cd SimpleITK-build/Documentation/
+
 tar -zcvf ${OUT_DIR}/simpleitk_doxygen_html_${SIMPLEITK_GIT_TAG}.tar.gz html
+
+cd ${here}
+
+if [ ! -e bld/SimpleITK-build/Documentation/xml/sitkImage_8h.xml ]
+then
+    echo "SimpleITK documentation target hasn't been built"
+    exit 1
+fi
+
+
+rm -f ${here}/SimpleITK/Wrapping/R/Packaging/SimpleITK/man/*.Rd
+
+cd ${here}/SimpleITK/Utilities/GenerateDocs/
+cp /work/bin/config_vars.sh .
+./SwigDocUpdate.sh
+
+cp ${here}/SimpleITK/Wrapping/Python/PythonDocstrings.i ${OUT_DIR}/
+cp ${here}/SimpleITK/Wrapping/Java/JavaDoc.i ${OUT_DIR}
+
+cd ${here}/SimpleITK/Wrapping/R/Packaging/SimpleITK/man
+tar -zcvf ${OUT_DIR}/sitkRdfiles_${SIMPLEITK_GIT_TAG}.tar.gz *.Rd
--- Utilities/GenerateDocs/docker/run.sh
+++ Utilities/GenerateDocs/docker/run.sh
@@ -3,7 +3,9 @@
 #
 # Genrates a SimpleITK Doxygen tarball for a tag
 #
-# Example usage: $ SIMPLEITK_GIT_TAG=v1.2.0 run.sh
+# Example usage: $ SIMPLEITK_GIT_TAG=v1.2.0 ./run.sh
+#
+# Example usage: $SIMPLEITK_GIT_TAG=v1.2.0 SIMPLEITK_GIT_REPO=https://github.com/SimpleITK/SimpleITK.git ./run.sh
 #
 # This script build build a docker image, then run the SimpleITK build step inorder to generate the Doxygen. By mounting the current directory onto the container a Doxygen tar-ball will be produced.
 
@@ -12,17 +14,15 @@ if [ -n "$ExternalData_OBJECT_STORES" -a -d "$ExternalData_OBJECT_STORES" ] ; th
     extra_args="-v ${ExternalData_OBJECT_STORES}:/var/io/.ExternalData -e ExternalData_OBJECT_STORES=/var/io/.ExternalData"
 fi
 
-if [ ! -z "${SIMPLEITK_GIT_TAG}" ] ; then
-    extra_args="${extra_args} -e SIMPLEITK_GIT_TAG=${SIMPLEITK_GIT_TAG}"
-fi
-
-
 echo " extra_args: "  ${extra_args}
 
 DOCKER_TAG=simpleitk_doxygen
 
 SIMPLEITK_GIT_TAG=${SIMPLEITK_GIT_TAG:-v1.2.0}
+SIMPLEITK_GIT_REPO=${SIMPLEITK_GIT_REPO:-https://github.com/SimpleITK/SimpleITK.git}
+
+docker_args="-e SIMPLEITK_GIT_TAG=${SIMPLEITK_GIT_TAG} -e SIMPLEITK_GIT_REPO=${SIMPLEITK_GIT_REPO} -e _USER=$(id -un) -e _USERID=$(id -u) -e_GROUPID=$(id -g)"
 
 docker build --pull=true --rm=true -t ${DOCKER_TAG} -f Dockerfile .
 
-docker run --rm -t -e SIMPLEITK_GIT_TAG=${SIMPLEITK_GIT_TAG} -e _USER=$(id -un)  -e _USERID=$(id -u)  -e_GROUPID=$(id -g) $extra_args -v $(pwd):/work/io  ${DOCKER_TAG}
+docker run --rm -t $docker_args $extra_args -v $(pwd):/work/io  ${DOCKER_TAG}
