fbc34dfffaad069c96c4e5620bad0decd07c8c0d
Bradley Lowekamp <blowekamp@mail.nih.gov>
2019-01-31 16:15:40 -0500

    Add user Applications to show search path
    When a user on Mac OSX does not have admin privileges, the Fiji.app
    should be placed in ~/Applications. Search for this too.

--- Code/IO/src/sitkImageViewer.cxx
+++ Code/IO/src/sitkImageViewer.cxx
@@ -174,6 +174,12 @@ void ImageViewer::initializeDefaults()
   m_GlobalDefaultSearchPath.push_back(  "/Developer/" );
   m_GlobalDefaultSearchPath.push_back(  "/opt/" );
   m_GlobalDefaultSearchPath.push_back(  "/usr/local/" );
+  std::string homedir;
+  if ( itksys::SystemTools::GetEnv ( "HOME", homedir ) )
+    {
+    m_GlobalDefaultSearchPath.push_back( homedir + "/Applications/" );
+    }
+
 
 #else
 
