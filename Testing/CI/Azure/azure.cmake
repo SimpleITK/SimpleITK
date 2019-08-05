81a9eb3cfdea327ff44e8ebc00e39d8af94f85d6
Bradley Lowekamp <blowekamp@mail.nih.gov>
2019-08-05 14:56:31 -0400

    Add dashboard_git_branch support to set branch to build

--- Testing/CI/Azure/azure.cmake
+++ Testing/CI/Azure/azure.cmake
@@ -24,7 +24,24 @@ endfunction()
 
 set_from_env(CTEST_SITE "AGENT_MACHINENAME" REQUIRED)
 set(CTEST_SITE "Azure.${CTEST_SITE}")
-set(CTEST_UPDATE_VERSION_ONLY 1)
+
+set_from_env(workspace "AGENT_BUILDDIRECTORY" REQUIRED)
+file(TO_CMAKE_PATH "${workspace}" CTEST_DASHBOARD_ROOT)
+
+set_from_env(dashboard_git_branch "DASHBOARD_GIT_BRANCH")
+if( DEFINED dashboard_git_branch)
+  set(CTEST_UPDATE_VERSION_ONLY 1)
+  set_from_env(CTEST_SOURCE_DIRECTORY "CTEST_SOURCE_DIRECTORY" DEFAULT "${CTEST_DASHBOARD_ROOT}/sitk")
+  if (EXISTS "${CTEST_SOURCE_DIRECTORY}")
+    message(WARNING "The source path \"${CTEST_SOURCE_DIRECTORY}\" already exists!")
+  endif()
+else()
+  set(CTEST_UPDATE_VERSION_ONLY 1)
+  file(RELATIVE_PATH dashboard_source_name "${workspace}" "$ENV{BUILD_SOURCESDIRECTORY}")
+  set_from_env(CTEST_SOURCE_DIRECTORY "BUILD_SOURCESDIRECTORY" REQUIRED)
+endif()
+set_from_env(CTEST_BINARY_DIRECTORY "CTEST_BINARY_DIRECTORY")
+
 
 include( ProcessorCount )
 ProcessorCount( _processor_count )
@@ -32,9 +49,6 @@ set_from_env(PARALLEL_LEVEL "PARALLEL_LEVEL" DEFAULT "${_processor_count}" )
 set( CTEST_TEST_ARGS ${CTEST_TEST_ARGS} PARALLEL_LEVEL ${PARALLEL_LEVEL})
 
 
-set_from_env(workspace "AGENT_BUILDDIRECTORY" REQUIRED)
-file(TO_CMAKE_PATH "${workspace}" CTEST_DASHBOARD_ROOT)
-file(RELATIVE_PATH dashboard_source_name "${workspace}" "$ENV{BUILD_SOURCESDIRECTORY}")
 
 # Make environment variables to CMake variables for CTest
 set_from_env(CTEST_CMAKE_GENERATOR "CTEST_CMAKE_GENERATOR" DEFAULT "Ninja" )
@@ -42,8 +56,6 @@ if (DEFINED ENV{CTEST_BUILD_CONFIGURATION})
   message(WARNING "Ignoring depricated variable CTEST_BUILD_CONFIGURATION")
 endif()
 set_from_env(CTEST_CONFIGURATION_TYPE "CTEST_CONFIGURATION_TYPE" DEFAULT "Release")
-set_from_env(CTEST_SOURCE_DIRECTORY "BUILD_SOURCESDIRECTORY" REQUIRED)
-#set_from_env(CTEST_BINARY_DIRECTORY "CTEST_BINARY_DIRECTORY")
 set_from_env(CTEST_BUILD_FLAGS "CTEST_BUILD_FLAGS")
 set_from_env(CTEST_BUILD_TARGET "CTEST_BUILD_TARGET")
 set_from_env(CTEST_TEST_ARGS "CTEST_TEST_ARGS")
