6f6655790e449c626f767af3d73ef4b7e32ec1ca
Bradley Lowekamp <blowekamp@mail.nih.gov>
2019-12-16 10:55:21 -0500

    WIP: Test Updating to current ITK master to test 5.1 pre-release

--- SuperBuild/External_ITK.cmake
+++ SuperBuild/External_ITK.cmake
@@ -52,7 +52,7 @@ set(ITK_GIT_REPOSITORY "${git_protocol}://github.com/InsightSoftwareConsortium/I
 mark_as_advanced(ITK_GIT_REPOSITORY)
 sitk_legacy_naming(ITK_GIT_REPOSITORY ITK_REPOSITORY)
 
-set(_DEFAULT_ITK_GIT_TAG "v5.1rc01")
+set(_DEFAULT_ITK_GIT_TAG "d4ff96b9ecf16716a22b5d29caabf6d4d4bdb36e")
 set(ITK_GIT_TAG "${_DEFAULT_ITK_GIT_TAG}" CACHE STRING "Tag in ITK git repo")
 mark_as_advanced(ITK_GIT_TAG)
 set(ITK_TAG_COMMAND GIT_TAG "${ITK_GIT_TAG}")
