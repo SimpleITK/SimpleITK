1034703ad0ccc08b376148d92f634fee4c8bfc33
Philip MacMenamin <philip.macmenamin@nih.gov>
2019-12-20 10:40:09 -0500

    Adds our favorite logo back

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -96,7 +96,7 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-#  html_logo= '../images/simpleitk_logo.png'
+html_logo= '../images/simpleitk_logo.png'
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
