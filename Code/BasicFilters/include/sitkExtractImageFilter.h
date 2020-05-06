f4438710fb8b7d581b1622171a7110a11970364d
Bradley Lowekamp <blowekamp@mail.nih.gov>
2020-05-06 09:58:04 -0400

    Update ExtractImageFilter documentation and formatting

--- Code/BasicFilters/include/sitkExtractImageFilter.h
+++ Code/BasicFilters/include/sitkExtractImageFilter.h
@@ -27,26 +27,47 @@ namespace itk {
   namespace simple {
 
     /**\class ExtractImageFilter
-\brief Decrease the image size by cropping the image to the selected region bounds.
-
-ExtractImageFilter changes the image boundary of an image by removing pixels outside the target region. The target region must be specified.
-
-ExtractImageFilter also collapses dimensions so that the input image may have more dimensions than the output image (i.e. 4-D input image to a 3-D output image). To specify what dimensions to collapse, the ExtractionRegion must be specified. For any dimension dim where ExtractionRegion.Size[dim] = 0, that dimension is collapsed. The index to collapse on is specified by ExtractionRegion.Index[dim]. For example, we have a image 4D = a 4x4x4x4 image, and we want to get a 3D image, 3D = a 4x4x4 image, specified as [x,y,z,2] from 4D (i.e. the 3rd "time" slice from 4D). The ExtractionRegion.Size = [4,4,4,0] and ExtractionRegion.Index = [0,0,0,2].
-
-The number of dimension in ExtractionRegion.Size and Index must = InputImageDimension. The number of non-zero dimensions in ExtractionRegion.Size must = OutputImageDimension.
-
-The output image produced by this filter will have the same origin as the input image, while the ImageRegion of the output image will start at the starting index value provided in the ExtractRegion parameter. If you are looking for a filter that will re-compute the origin of the output image, and provide an output image region whose index is set to zeros, then you may want to use the RegionOfInterestImageFilter . The output spacing is is simply the collapsed version of the input spacing.
-
-Determining the direction of the collapsed output image from an larger dimensional input space is an ill defined problem in general. It is required that the application developer select the desired transformation strategy for collapsing direction cosines. It is REQUIRED that a strategy be explicitly requested (i.e. there is no working default). Direction Collapsing Strategies: 1) DirectionCollapseToUnknown(); This is the default and the filter can not run when this is set. The reason is to explicitly force the application developer to define their desired behavior. 1) DirectionCollapseToIdentity(); Output has identity direction no matter what 2) DirectionCollapseToSubmatrix(); Output direction is the sub-matrix if it is positive definite, else throw an exception.
-
-This filter is implemented as a multithreaded filter. It provides a DynamicThreadedGenerateData() method for its implementation.
-
-\note This filter is derived from InPlaceImageFilter . When the input to this filter matched the output requested region, like with streaming filter for input, then setting this filter to run in-place will result in no copying of the bulk pixel data.
-
-
-\see CropImageFilter
-\sa itk::simple::Extract for the procedural interface
-\sa itk::ExtractImageFilter<InputImageType, typename InputImageType::template Rebind for the Doxygen on the original ITK class.
+     * \brief Decrease the image size by cropping the image to the selected region bounds.
+     *
+     * ExtractImageFilter changes the image boundary of an image by
+     * removing pixels outside the target region. The region is
+     * specified as a Size and Index. The Size must
+     * be specified, while the Index defaults to zeros.
+     *
+     * ExtractImageFilter can collapses dimensions so that the input
+     * image may have more dimensions than the output image (i.e. 4-D
+     * input image to a 3-D output image). To specify what dimensions
+     * to collapse, the Size must be specified. For any dimension dim
+     * where the Size[dim] == 0, that dimension is collapsed. The
+     * index to collapse on is specified by Index[dim]. For example,
+     * we have a image 4D = a 4x4x4x4 image, and we want to get a 3D
+     * image, 3D = a 4x4x4 image, specified as [x,y,z,2] from 4D
+     * (i.e. the 3rd "time" slice from 4D). The Size = [4,4,4,0] and
+     * Index = [0,0,0,2].
+     *
+     * The number of dimension in Size and Index must at least
+     * dimension of the input image. The number of non-zero dimensions
+     * in Size determines the output dimension.
+     *
+     * Determining the direction of the collapsed output image from an
+     * larger dimensional input space is an ill defined problem in
+     * general. It is required that the application developer select
+     * the desired transformation strategy for collapsing direction
+     * cosines. The strategy defaults to the guess approach. Direction
+     * Collapsing Strategies: 1) DirectionCollapseToUnknown(); This is
+     * the default in ITK and the filter can not run when this is
+     * set. 1) DirectionCollapseToIdentity(); Output has identity
+     * direction no matter what 2) DirectionCollapseToSubmatrix();
+     * Output direction is the sub-matrix if it is positive definite,
+     * else throw an exception.
+     *
+     * This filter is implemented as a multithreaded filter. It
+     * provides a DynamicThreadedGenerateData() method for its
+     * implementation.
+     *
+     * \see CropImageFilter
+     * \sa itk::simple::Extract for the procedural interface
+     * \sa itk::ExtractImageFilter for the Doxygen on the original ITK class.
      */
     class SITKBasicFilters_EXPORT ExtractImageFilter : public ImageFilter {
     public:
@@ -61,45 +82,64 @@ This filter is implemented as a multithreaded filter. It provides a DynamicThrea
 
       /** Define the pixels types supported by this filter */
       using PixelIDTypeList = NonLabelPixelIDTypeList;
-\
 
-      /**
+      /** \brief Set the size of the region to extract.
+       *
+       * The size of the region to extract should be
+       * specified. Dimensions which have a size of 0 are
+       * collapsed. The number of non-zero sized determines the output
+       * dimension.
        */
       SITK_RETURN_SELF_TYPE_HEADER SetSize ( std::vector<unsigned int> Size ) { this->m_Size = std::move(Size); return *this; }
 
-      /**
+      /** \brief Get the size of the region to extract.
        */
-      std::vector<unsigned int> GetSize() const { return this->m_Size; }\
+      std::vector<unsigned int> GetSize() const { return this->m_Size; }
 
-      /**
-       * 	odo the internal setting of the method needs work!!!
+      /** \brief Set the starting index of the input image to extract.
+       *
+       * The index defaults to all zeros.
        */
       SITK_RETURN_SELF_TYPE_HEADER SetIndex ( std::vector<int> Index ) { this->m_Index = std::move(Index); return *this; }
 
-      /**
+      /** \brief Get the starting index to extract.
        */
       std::vector<int> GetIndex() const { return this->m_Index; }
 
-      typedef enum {DIRECTIONCOLLAPSETOUNKOWN,DIRECTIONCOLLAPSETOIDENTITY,DIRECTIONCOLLAPSETOSUBMATRIX,DIRECTIONCOLLAPSETOGUESS} DirectionCollapseToStrategyType;\
+      typedef enum {DIRECTIONCOLLAPSETOUNKOWN,DIRECTIONCOLLAPSETOIDENTITY,DIRECTIONCOLLAPSETOSUBMATRIX,DIRECTIONCOLLAPSETOGUESS} DirectionCollapseToStrategyType;
 
-      /**
-       * Backwards compatibility for enum values Set the strategy to be used to collapse physical space dimensions.
-
-itk::itkExtractImageFilter::DIRECTIONCOLLAPSETOIDENTITY Set the strategy so that all collapsed images have an identity direction. Use this strategy when you know that retention of the physical space orientation of the collapsed image is not important.
-
-itk::itkExtractImageFilter::DIRECTIONCOLLAPSETOGUESS Set the strategy so that all collapsed images where output direction is the sub-matrix if it is positive definite, else return identity. This is backwards compatible with ITKv3, but is highly discouraged because the results are difficult to anticipate under differing data scenarios.
-
-itk::itkExtractImageFilter::DIRECTIONCOLLAPSETOSUBMATRIX Set the strategy so that all collapsed images where output direction is the sub-matrix if it is positive definite, else throw an exception. Use this strategy when it is known that properly identified physical space sub-volumes can be reliably extracted from a higher dimensional space. For example when the application programmer knows that a 4D image is 3D+time, and that the 3D sub-space is properly defined.
+      /** \brief Set the strategy to be used to collapse physical space dimensions.
+       *
+       * DIRECTIONCOLLAPSETOIDENTITY Set
+       * the strategy so that all collapsed images have an identity
+       * direction. Use this strategy when you know that retention of
+       * the physical space orientation of the collapsed image is not
+       * important.
+       *
+       * DIRECTIONCOLLAPSETOGUESS Set the
+       * strategy so that all collapsed images where output direction
+       * is the sub-matrix if it is positive definite, else return
+       * identity. This is backwards compatible with ITKv3, but is
+       * highly discouraged because the results are difficult to
+       * anticipate under differing data scenarios.
+       *
+       * DIRECTIONCOLLAPSETOSUBMATRIX Set the strategy so that all
+       * collapsed images where output direction is the sub-matrix if
+       * it is positive definite, else throw an exception. Use this
+       * strategy when it is known that properly identified physical
+       * space sub-volumes can be reliably extracted from a higher
+       * dimensional space. For example when the application
+       * programmer knows that a 4D image is 3D+time, and that the 3D
+       * sub-space is properly defined.
        */
       SITK_RETURN_SELF_TYPE_HEADER SetDirectionCollapseToStrategy ( DirectionCollapseToStrategyType DirectionCollapseToStrategy ) { this->m_DirectionCollapseToStrategy = DirectionCollapseToStrategy; return *this; }
 
-      /**
-       * NOTE: The SetDirectionCollapseToUknown is explicitly not defined. It is a state that a filter can be in only when it is first instantiate prior to being initialized. Get the currently set strategy for collapsing directions of physical space.
+      /** Get the currently set strategy for collapsing directions of physical space.
        */
       DirectionCollapseToStrategyType GetDirectionCollapseToStrategy() const { return this->m_DirectionCollapseToStrategy; }
 
       /** Name of this class */
-      std::string GetName() const { return std::string ("ExtractImageFilter"); }
+      std::string GetName() const { return "ExtractImageFilter"; }
 
       /** Print ourselves out */
       std::string ToString() const;
