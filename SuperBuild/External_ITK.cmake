f76ae3cefab8abbb9394387a260c2c8da1f07d11
Bradley Lowekamp <blowekamp@mail.nih.gov>
2022-02-08 17:11:15 +0000

    Update ITK after 5.3rc03, with eigen warning fixes

--- SuperBuild/External_ITK.cmake
+++ SuperBuild/External_ITK.cmake
@@ -62,7 +62,7 @@ set(ITK_GIT_REPOSITORY "${git_protocol}://github.com/InsightSoftwareConsortium/I
 mark_as_advanced(ITK_GIT_REPOSITORY)
 sitk_legacy_naming(ITK_GIT_REPOSITORY ITK_REPOSITORY)
 
-set(_DEFAULT_ITK_GIT_TAG "v5.3rc03")
+set(_DEFAULT_ITK_GIT_TAG "1639cab20ba52dc49118d96fef881bfa72d58551")
 set(ITK_GIT_TAG "${_DEFAULT_ITK_GIT_TAG}" CACHE STRING "Tag in ITK git repo")
 mark_as_advanced(ITK_GIT_TAG)
 set(ITK_TAG_COMMAND GIT_TAG "${ITK_GIT_TAG}")
