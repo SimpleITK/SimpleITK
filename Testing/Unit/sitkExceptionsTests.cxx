ab254e025165805b7f3b561d0e16dadb14aefd4e
Niels Dekker <N.Dekker@lumc.nl>
2022-07-22 19:00:09 +0200

    Add `sitkExceptionsTest.TestDestructionAfterAssignment` unit test
    Aims to reproduce a potential crash in the assignment operator of GenericException (SimpleITK version <= v2.2rc3).

--- Testing/Unit/sitkExceptionsTests.cxx
+++ Testing/Unit/sitkExceptionsTests.cxx
@@ -109,3 +109,21 @@ TEST_F(sitkExceptionsTest, Test3) {
   EXPECT_NO_THROW( empty.what() );
 
 }
+
+
+TEST_F(sitkExceptionsTest, TestDestructionAfterAssignment) {
+
+    // Test that the program does not crash when two GenericException
+    // variables get destructed (automatically, by getting out of scope),
+    // when one of them is assigned to the other. (Such a crash might occur
+    // with previous versions of SimpleITK, version <= v2.2rc3.)
+
+    const itk::simple::GenericException source(__FILE__, __LINE__);
+    itk::simple::GenericException target;
+
+    target = source;
+
+    // After assignment, the variables should compare equal, but moreover,
+    // when they get out of scope, their destructors should not crash.
+    EXPECT_EQ(target, source);
+}
