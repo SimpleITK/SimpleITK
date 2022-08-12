f4633c4208ace26a7a3f65306b916d959f1e9bcb
Bradley Lowekamp <blowekamp@mail.nih.gov>
2022-08-12 12:32:32 +0000

    JSONValidate explicitly open with UTF-8 encoding

--- Utilities/JSON/JSONValidate.py
+++ Utilities/JSON/JSONValidate.py
@@ -2,7 +2,7 @@
 import json, sys
 
 try:
-    json.load( open( sys.argv[1], "r" ))
+    json.load( open( sys.argv[1], "r", encoding='utf-8' ))
 except ValueError as e:
     print( "ValueError: {0}".format( e ) )
     print( "{0}:0:0: error: validating json file.".format( sys.argv[1] ) )
