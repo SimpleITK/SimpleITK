385227ed4c00e9a870c735ee84420a2bd2fea857
Mike Bopf <mbopf@mail.nih.gov>
2022-12-01 12:38:16 -0500

    Changed language = None to 'en' since it was throwing a warning.
    No issue number yet.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -64,7 +64,9 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+# Setting default to English to avoid warning.
+#language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
