5c50eacc7e17e73c62261362f7a9e06487aa4c23
Mike Bopf <mbopf@mail.nih.gov>
2022-12-05 17:42:32 -0500

    Moved description from code comments into the .rst Overview section.
    Fixes #1768.

--- Examples/ImageRegistrationMethodExhaustive/ImageRegistrationMethodExhaustive.R
+++ Examples/ImageRegistrationMethodExhaustive/ImageRegistrationMethodExhaustive.R
@@ -20,21 +20,6 @@
 # Rscript --vanilla ImageRegistrationMethodExhaustive.R fixedImageFilter movingImageFile outputTransformFile
 #
 
-# This script demonstrates the use of the Exhaustive optimizer in the
-# ImageRegistrationMethod to estimate a good initial rotation position.
-
-# Because gradient descent base optimization can get stuck in local
-# minima, a good initial transform is critical for reasonable
-# results. Search a reasonable space on a grid with brute force may be a
-# reliable way to get a starting location for further optimization.
-
-# The initial translation and center of rotation for the transform is
-# initialized based on the first principle moments of the intensities of
-# the image. Then in either 2D or 3D a Euler transform is used to
-# exhaustively search a grid of the rotation space at a certain step
-# size. The resulting transform is a reasonable guess where to start
-# further registration.
-
 library(SimpleITK)
 
 commandIteration <- function(method)
