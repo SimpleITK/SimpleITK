dbeb96c011b53a0d81d31d60cfa01d949f2cd704
Mike Bopf <mbopf@mail.nih.gov>
2023-01-13 14:34:20 -0500

    Used matplotlib Sphinx plot directive to embed output image in the docs.
    First fix for #1797

--- Examples/HelloWorld/HelloWorld.py
+++ Examples/HelloWorld/HelloWorld.py
@@ -20,44 +20,51 @@
 import os
 import SimpleITK as sitk
 
-# Create an image
-pixelType = sitk.sitkUInt8
-imageSize = [128, 128]
-image = sitk.Image(imageSize, pixelType)
 
-# Create a face image
-faceSize = [64, 64]
-faceCenter = [64, 64]
-face = sitk.GaussianSource(pixelType, imageSize, faceSize, faceCenter)
+def main():
+    # Create an image
+    pixelType = sitk.sitkUInt8
+    imageSize = [128, 128]
+    image = sitk.Image(imageSize, pixelType)
 
-# Create eye images
-eyeSize = [5, 5]
-eye1Center = [48, 48]
-eye2Center = [80, 48]
-eye1 = sitk.GaussianSource(pixelType, imageSize, eyeSize, eye1Center, 150)
-eye2 = sitk.GaussianSource(pixelType, imageSize, eyeSize, eye2Center, 150)
+    # Create a face image
+    faceSize = [64, 64]
+    faceCenter = [64, 64]
+    face = sitk.GaussianSource(pixelType, imageSize, faceSize, faceCenter)
 
-# Apply the eyes to the face
-face = face - eye1 - eye2
-face = sitk.BinaryThreshold(face, 200, 255, 255)
+    # Create eye images
+    eyeSize = [5, 5]
+    eye1Center = [48, 48]
+    eye2Center = [80, 48]
+    eye1 = sitk.GaussianSource(pixelType, imageSize, eyeSize, eye1Center, 150)
+    eye2 = sitk.GaussianSource(pixelType, imageSize, eyeSize, eye2Center, 150)
 
-# Create the mouth
-mouthRadii = [30, 20]
-mouthCenter = [64, 76]
-mouth = 255 - sitk.BinaryThreshold(
-    sitk.GaussianSource(pixelType, imageSize, mouthRadii, mouthCenter),
-    200,
-    255,
-    255,
-)
-# Paste the mouth into the face
-mouthSize = [64, 18]
-mouthLoc = [32, 76]
-face = sitk.Paste(face, mouth, mouthSize, mouthLoc, mouthLoc)
+    # Apply the eyes to the face
+    face = face - eye1 - eye2
+    face = sitk.BinaryThreshold(face, 200, 255, 255)
 
-# Apply the face to the original image
-image = image + face
+    # Create the mouth
+    mouthRadii = [30, 20]
+    mouthCenter = [64, 76]
+    mouth = 255 - sitk.BinaryThreshold(
+        sitk.GaussianSource(pixelType, imageSize, mouthRadii, mouthCenter),
+        200,
+        255,
+        255,
+    )
+    # Paste the mouth into the face
+    mouthSize = [64, 18]
+    mouthLoc = [32, 76]
+    face = sitk.Paste(face, mouth, mouthSize, mouthLoc, mouthLoc)
+
+    # Apply the face to the original image
+    image = image + face
+
+    return image
+
 
 # Display the results
-if "SITK_NOSHOW" not in os.environ:
-    sitk.Show(image, title="Hello World: Python", debugOn=True)
+if __name__ == "__main__":
+    if "SITK_NOSHOW" not in os.environ:
+        img = main()
+        sitk.Show(img, title="Hello World: Python", debugOn=True)
