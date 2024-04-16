c1c43c56d6437e673de8efe2a88e8614290f5884
Bradley Lowekamp <blowekamp@mail.nih.gov>
2024-04-16 17:01:06 -0400

    Add windows wheel packaging on GHA

--- .github/workflows/scripts/win_build_python.sh
+++ .github/workflows/scripts/win_build_python.sh
@@ -7,6 +7,7 @@ export PATH=/usr/bin:${PATH}
 export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=2
 
 echo "COREBINARYDIRECTORY: ${COREBINARYDIRECTORY}"
+echo "CTEST_SOURCE_DIRECTORY: ${CTEST_SOURCE_DIRECTORY}"
 
 python --version
 PYTHON_VERSION=$(python -c 'import sys;print ("{0}{1}".format(sys.version_info[0], sys.version_info[1]))')
@@ -27,19 +28,19 @@ EOM
 
 
 export CTEST_CACHE
-export CTEST_BINARY_DIRECTORY="${AGENT_BUILDDIRECTORY}/py${PYTHON_VERSION}"
+export CTEST_BINARY_DIRECTORY="${GITHUB_WORKSPACE}/py${PYTHON_VERSION}"
 
 export CC=cl.exe
 export CXX=cl.exe
 
 ctest -D dashboard_source_config_dir="Wrapping/Python" \
       -D "dashboard_track:STRING=Package" \
-      -D "CTEST_BUILD_NAME:STRING=${AGENT_NAME}-${AGENT_JOBNAME}-py${PYTHON_VERSION}" \
+      -D "CTEST_BUILD_NAME:STRING=${RUNNER_NAME}-${GITHUB_JOB}-py${PYTHON_VERSION}" \
       -D "CTEST_CMAKE_GENERATOR:STRING=Ninja" \
-      -S ${BUILD_SOURCESDIRECTORY}/Testing/CI/Azure/azure.cmake -V || true
+      -S ${CTEST_SOURCE_DIRECTORY}/.github/workflows/github_actions.cmake -VV -j 2
 
 ( cd ${CTEST_BINARY_DIRECTORY} && cmake --build "${CTEST_BINARY_DIRECTORY}" --config "${CTEST_CONFIGURATION_TYPE}" --target dist )
 
 
-mkdir -p "${BUILD_ARTIFACTSTAGINGDIRECTORY}/python"
-find ${CTEST_BINARY_DIRECTORY} -name "SimpleITK*.whl" -exec cp -v {} "${BUILD_ARTIFACTSTAGINGDIRECTORY}/python" \;
+mkdir -p "${GITHUB_WORKSPACE}/artifacts"
+find ${CTEST_BINARY_DIRECTORY} -name "SimpleITK*.whl" -exec cp -v {} "${GITHUB_WORKSPACE}/artifacts" \;
