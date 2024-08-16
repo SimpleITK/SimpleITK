459261649138a8e42fedebd715a826f5ccdb1915
Bradley Lowekamp <blowekamp@mail.nih.gov>
2024-08-15 20:16:12 -0400

    Use SimpleITK_BUILD_DISTRIBUTE in Mac ARM build scrip

--- Utilities/Distribution/MacARM/mac_build_core.sh
+++ Utilities/Distribution/MacARM/mac_build_core.sh
@@ -15,6 +15,7 @@ cmake \
     -D "CMAKE_BUILD_TYPE:STRING=MinSizeRel" \
     -D "CMAKE_OSX_DEPLOYMENT_TARGET=11.0" \
     -D "MAKE_OSX_SYSROOT:PATH=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.3.sdk" \
+    -D "SimpleITK_BUILD_DISTRIBUTE:BOOL=ON" \
     -D "BUILD_EXAMPLES:BOOL=OFF" \
     -D "BUILD_TESTING:BOOL=ON" \
     -D "BUILD_SHARED_LIBS:BOOL=OFF" \
