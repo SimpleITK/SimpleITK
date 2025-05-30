0b7cbbc9da099aaba32d267a06f0fa7812d809b1
Bradley Lowekamp <blowekamp@mail.nih.gov>
2025-05-30 14:35:08 -0400

    Fix CMake version policy max is now correctly 3.20

--- CMake/sitkCMakeInit.cmake
+++ CMake/sitkCMakeInit.cmake
@@ -1,5 +1,5 @@
 set(SITK_CMAKE_MINIMUM_REQUIRED_VERSION "3.16.3")
-set(SITK_CMAKE_POLICY_VERSION "${SITK_CMAKE_MINIMUM_REQUIRED_VERSION}...3.20")
+set(SITK_CMAKE_POLICY_VERSION "3.20")
 
 # cache_list_append( <cache list name> [<element> ...])
 #
