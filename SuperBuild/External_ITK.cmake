392ab24ba29397a81b6e2e9f2a4ad61b85c4e6f9
Bradley Lowekamp <blowekamp@mail.nih.gov>
2025-06-11 12:32:33 -0400

    Update ITK Superbuild version to 5.4.4

--- SuperBuild/External_ITK.cmake
+++ SuperBuild/External_ITK.cmake
@@ -67,8 +67,8 @@ mark_as_advanced(ITK_GIT_REPOSITORY)
 sitk_legacy_naming(ITK_GIT_REPOSITORY ITK_REPOSITORY)
 
 
-# ITK v5.4.3 tag
-set(_DEFAULT_ITK_GIT_TAG "v5.4.3")
+# ITK v5.4.4 tag
+set(_DEFAULT_ITK_GIT_TAG "v5.4.4")
 set(ITK_GIT_TAG "${_DEFAULT_ITK_GIT_TAG}" CACHE STRING "Tag in ITK git repo")
 mark_as_advanced(ITK_GIT_TAG)
 set(ITK_TAG_COMMAND GIT_TAG "${ITK_GIT_TAG}")
