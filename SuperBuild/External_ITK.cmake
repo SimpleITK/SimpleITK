d216f1ba5877eb24aee262ae9b644002d697ef90
Bradley Lowekamp <blowekamp@mail.nih.gov>
2025-09-23 10:17:24 -0400

    Update ITK version to v6.0b01

--- SuperBuild/External_ITK.cmake
+++ SuperBuild/External_ITK.cmake
@@ -106,8 +106,8 @@ set(
 mark_as_advanced(ITK_GIT_REPOSITORY)
 sitk_legacy_naming(ITK_GIT_REPOSITORY ITK_REPOSITORY)
 
-# Using ITK 6.0 developement branch as of Aug 21, 2025
-set(_DEFAULT_ITK_GIT_TAG "5d6cc1979085e22a401ad5a0142b9f4ad1374d81")
+# Using ITK 6.0 Beta 1
+set(_DEFAULT_ITK_GIT_TAG "v6.0b01")
 set(ITK_GIT_TAG "${_DEFAULT_ITK_GIT_TAG}" CACHE STRING "Tag in ITK git repo")
 mark_as_advanced(ITK_GIT_TAG)
 set(
